746a693deabe1ba35989685fc80ae8c5905f1260
Jose E. Roman <jroman@dsic.upv.es>
2004-03-10 18:10:10 +0000

    Fix default value of ncv in EPSSUBSPACE
    (Logical change 1.24)
    
    
    Subversion-commit: r71

--- src/eps/impls/subspace/subspace.c
+++ src/eps/impls/subspace/subspace.c
@@ -32,7 +32,7 @@ static int EPSSetDefaults_SUBSPACE(EPS eps)
   if (eps->ncv) {
     if (eps->ncv<eps->nev) SETERRQ(1,"The value of ncv must be at least nev"); 
   }
-  else eps->ncv = PetscMax(2*eps->nev,eps->nev+8);
+  else eps->ncv = PetscMin(2*eps->nev,eps->nev+8);
   if (!eps->max_it) eps->max_it = PetscMax(100,N);
   if (!subspace->inner) {
     if (eps->ishermitian) subspace->inner = 10;
