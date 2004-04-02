4293492a1a2c7db0a6e153a88ce23fa61a44381b
Andres Tomas <antodo@upvnet.upv.es>
2004-04-02 14:43:29 +0000

    Removed EPSSIROrhogonalization. Renamed EPSUnmodifiedGramSchmidtOrthogonalization to EPSClassicalGramSchmidtOrthogonalization, which now does a second step refinement if needed.
    (Logical change 1.52)
    
    
    Subversion-commit: r150

--- src/eps/interface/borthog.c
+++ src/eps/interface/borthog.c
@@ -13,7 +13,7 @@
  */
 #undef __FUNCT__  
 #define __FUNCT__ "EPSModifiedGramSchmidtOrthogonalization"
-int EPSModifiedGramSchmidtOrthogonalization(EPS eps,int it,PetscScalar *H)
+int EPSModifiedGramSchmidtOrthogonalization(EPS eps,int it,PetscScalar *H,PetscReal *norm)
 {
   int         ierr,j;
   PetscScalar alpha;
@@ -28,6 +28,7 @@ int EPSModifiedGramSchmidtOrthogonalization(EPS eps,int it,PetscScalar *H)
     alpha = -alpha;
     ierr = VecAXPY(&alpha,eps->V[j],eps->V[it+1]);CHKERRQ(ierr);
   }
+  ierr = VecNorm(eps->V[it+1], NORM_2, norm);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
@@ -37,56 +38,19 @@ int EPSModifiedGramSchmidtOrthogonalization(EPS eps,int it,PetscScalar *H)
   when running in a parallel environment.
  */
 #undef __FUNCT__  
-#define __FUNCT__ "EPSUnmodifiedGramSchmidtOrthogonalization"
-int EPSUnmodifiedGramSchmidtOrthogonalization(EPS eps,int it,PetscScalar *H)
+#define __FUNCT__ "EPSClassicalGramSchmidtOrthogonalization"
+int EPSClassicalGramSchmidtOrthogonalization(EPS eps,int it,PetscScalar *H,PetscReal *norm)
 {
   int         ierr,j;
-  PetscScalar shh[100];
-  PetscTruth  alloc = PETSC_FALSE;
-
-  PetscFunctionBegin;
-
-  if (!H) {
-    if (it<10) H = shh;   /* Don't allocate small arrays */
-    else { 
-      ierr = PetscMalloc((it+1)*(it+1)*sizeof(PetscScalar),&H);CHKERRQ(ierr);
-      alloc = PETSC_TRUE;
-    }
-  }
-
-  /* This is really a matrix-vector product, with the matrix stored
-     as pointer to rows */
-  ierr = VecMDot(it+1,eps->V[it+1],eps->V,H);CHKERRQ(ierr);
-
-  /* This is really a matrix-vector product: 
-     [h_0,h_1,...]*[ v_0; v_1; ...] subtracted from v_{it+1}.  */
-  for (j=0;j<=it;j++) H[j] = -H[j];
-  ierr = VecMAXPY(it+1,H,eps->V[it+1],eps->V);CHKERRQ(ierr);
-  for (j=0;j<=it;j++) H[j] = -H[j];
-
-  if (alloc) { ierr = PetscFree(H);CHKERRQ(ierr); }
-  PetscFunctionReturn(0);
-}
-
-/*
-  This version uses 1 iteration of iterative refinement of UNMODIFIED Gram-Schmidt.  
-  It can give better performance when running in a parallel 
-  environment and in some cases even in a sequential environment (because
-  MAXPY has more data reuse).
- */
-#undef __FUNCT__  
-#define __FUNCT__ "EPSIROrthogonalization"
-int EPSIROrthogonalization(EPS eps,int it,PetscScalar *H)
-{
-  int         ierr,j,ncnt;
   PetscScalar shh[100],shh2[100],*lhh;
   PetscTruth  alloc = PETSC_FALSE;
+  PetscReal   hnorm;
 
   PetscFunctionBegin;
   if (!H) {
     if (it<10) H = shh2;   /* Don't allocate small arrays */
     else { 
-      ierr = PetscMalloc((it+1)*(it+1)*sizeof(PetscScalar),&H);CHKERRQ(ierr);
+      ierr = PetscMalloc((it+1)*sizeof(PetscScalar),&H);CHKERRQ(ierr);
       alloc = PETSC_TRUE;
     }
   }
@@ -94,23 +58,47 @@ int EPSIROrthogonalization(EPS eps,int it,PetscScalar *H)
   if (it<100) lhh = shh;
   else { ierr = PetscMalloc((it+1)*sizeof(PetscScalar),&lhh);CHKERRQ(ierr); }
   
-  /* Clear H since we will accumulate values into it */
-  for (j=0;j<=it;j++) H[j] = 0.0;
+  /*** FIRST ITERATION ***/
 
-  ncnt = 0;
-  do {
-    /* This is really a matrix-vector product, with the matrix stored
-       as pointer to rows */
-    ierr = VecMDot(it+1,eps->V[it+1],eps->V,lhh);CHKERRQ(ierr); /* <v,vnew> */
+  /* This is really a matrix-vector product, with the matrix stored
+     as pointer to rows */
+  ierr = VecMDot(it+1,eps->V[it+1],eps->V,lhh);CHKERRQ(ierr); /* <v,vnew> */
 
-    /* This is really a matrix vector product: 
-       [h_0,h_1,...]*[ v_0; v_1; ...] subtracted from v_{it+1}.  */
-    for (j=0;j<=it;j++) lhh[j] = -lhh[j];
-    ierr = VecMAXPY(it+1,lhh,eps->V[it+1],eps->V);CHKERRQ(ierr);
-    for (j=0;j<=it;j++) {
-      H[j] -= lhh[j];     /* H += -<v,vnew> */
+  /* This is really a matrix vector product: 
+     [h_0,h_1,...]*[ v_0; v_1; ...] subtracted from v_{it+1}.  */
+  for (j=0;j<=it;j++) {
+    H[j] = lhh[j];     /* H = <v,vnew> */
+    lhh[j] = -lhh[j];    
+  }
+  ierr = VecMAXPY(it+1,lhh,eps->V[it+1],eps->V);CHKERRQ(ierr);
+  
+  ierr = VecNorm(eps->V[it+1], NORM_2, norm);CHKERRQ(ierr);
+
+  /*** SECOND ITERATION ***/
+  if (eps->orth_eta != 0.0) {
+    hnorm = 0.0;
+    for (j=0; j<=it; j++) {
+      hnorm  +=  PetscRealPart(H[j] * PetscConj(H[j]));
     }
-  } while (ncnt++ < 2);
+    hnorm = sqrt(hnorm);
+    if (eps->orth_eta < 0 || *norm < eps->orth_eta * hnorm) {
+      PetscLogInfo(eps,"EPSClassicalGramSchmidtOrthogonalization:Performing iterative refinement wnorm %g hnorm %g\n",*norm,hnorm);
+      
+      /* This is really a matrix-vector product, with the matrix stored
+         as pointer to rows */
+      ierr = VecMDot(it+1,eps->V[it+1],eps->V,lhh);CHKERRQ(ierr); /* <v,vnew> */
+
+      /* This is really a matrix vector product: 
+         [h_0,h_1,...]*[ v_0; v_1; ...] subtracted from v_{it+1}.  */
+      for (j=0;j<=it;j++) {
+        H[j] += lhh[j];     /* H += <v,vnew> */
+        lhh[j] = -lhh[j];    
+      }
+      ierr = VecMAXPY(it+1,lhh,eps->V[it+1],eps->V);CHKERRQ(ierr);
+
+      ierr = VecNorm(eps->V[it+1], NORM_2, norm);CHKERRQ(ierr);
+    }
+  }
 
   if (it>=100) { ierr = PetscFree(lhh);CHKERRQ(ierr); }
   if (alloc) { ierr = PetscFree(H);CHKERRQ(ierr); }
