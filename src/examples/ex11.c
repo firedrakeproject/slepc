05951d2f4e7b4df659ac43f8111439cdc64dd00b
Andres Tomas <antodo@upvnet.upv.es>
2004-05-11 09:35:42 +0000

    Added support and example for deflation space in subspace and power.
    2004/05/11 11:01:10+02:00 dsic.upv.es!antodo
    
    (Logical change 1.110)
    
    
    Subversion-commit: r290

---
+++ src/examples/ex11.c
@@ -0,0 +1,207 @@
+
+static char help[] = "Solves a standard symmetric eigenproblem corresponding to the Laplacian operator in 1 dimension.\n\n"
+  "The command line options are:\n\n"
+  "  -n <n>, where <n> = number of grid subdivisions = matrix dimension.\n\n";
+
+#include "slepceps.h"
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main( int argc, char **argv )
+{
+  Mat         A;               /* operator matrix */
+  EPS         eps;             /* eigenproblem solver context */
+  EPSType     type;
+  PetscReal   error, tol, re, im;
+  PetscScalar kr, ki;
+  int         n=30, nev, ierr, maxit, i, its, nconv, 
+              col[3], Istart, Iend, FirstBlock=0, LastBlock=0;
+  PetscScalar value[3];
+  Vec         v;
+
+  SlepcInitialize(&argc,&argv,(char*)0,help);
+
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-n",&n,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\n1-D Laplacian Eigenproblem, n=%d\n\n",n);
+         CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     Compute the operator matrix that defines the eigensystem, Ax=kx
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  ierr = MatCreate(PETSC_COMM_WORLD,PETSC_DECIDE,PETSC_DECIDE,n,n,&A);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(A);CHKERRQ(ierr);
+  
+  ierr = MatGetOwnershipRange(A,&Istart,&Iend);CHKERRQ(ierr);
+  if (Istart==0) FirstBlock=PETSC_TRUE;
+  if (Iend==n) LastBlock=PETSC_TRUE;
+  value[0]=-1.0; value[1]=2.0; value[2]=-1.0;
+  for( i=(FirstBlock? Istart+1: Istart); i<(LastBlock? Iend-1: Iend); i++ ) {
+    col[0]=i-1; col[1]=i; col[2]=i+1;
+    ierr = MatSetValues(A,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+  if (LastBlock) {
+    i=n-1; col[0]=n-2; col[1]=n-1;
+    ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+  if (FirstBlock) {
+    i=0; col[0]=0; col[1]=1; value[0]=2.0; value[1]=-1.0;
+    ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+
+  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                Create the eigensolver and set various options
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* 
+     Create eigensolver context
+  */
+  ierr = EPSCreate(PETSC_COMM_WORLD,&eps);CHKERRQ(ierr);
+
+  /* 
+     Set operators. In this case, it is a standard eigenvalue problem
+  */
+  ierr = EPSSetOperators(eps,A,PETSC_NULL);CHKERRQ(ierr);
+
+  /*
+     Set solver parameters at runtime
+  */
+  ierr = EPSSetFromOptions(eps);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                      Solve the eigensystem
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  ierr = EPSSolve(eps);CHKERRQ(ierr);
+  ierr = EPSGetIterationNumber(eps, &its);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of iterations of the method: %d\n",its);
+         CHKERRQ(ierr);
+  /*
+     Optional: Get some information from the solver and display it
+  */
+  ierr = EPSGetType(eps,&type);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Solution method: %s\n\n",type);CHKERRQ(ierr);
+  ierr = EPSGetDimensions(eps,&nev,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of requested eigenvalues: %d\n",nev);
+         CHKERRQ(ierr);
+  ierr = EPSGetTolerances(eps,&tol,&maxit);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Stopping condition: tol=%.4g, maxit=%d\n",tol,maxit);
+         CHKERRQ(ierr);
+  ierr = EPSGetConverged(eps,&nconv);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of converged eigenpairs: %d\n\n",nconv);
+         CHKERRQ(ierr);
+
+  if (nconv>0) {
+    /*
+       Display eigenvalues and relative errors
+    */
+    ierr = PetscPrintf(PETSC_COMM_WORLD,
+         "           k          ||Ax-kx||/||kx||\n"
+         "   ----------------- ------------------\n" );CHKERRQ(ierr);
+
+    ierr = MatGetVecs(A,&v,PETSC_NULL);CHKERRQ(ierr);
+    for( i=0; i<nconv; i++ ) {
+      /* 
+        Get converged eigenpairs: i-th eigenvalue is stored in kr (real part) and
+        ki (imaginary part)
+      */
+      ierr = EPSGetEigenpair(eps,i,&kr,&ki,v,PETSC_NULL);CHKERRQ(ierr);
+      ierr = EPSAttachDeflationSpace(eps, 1, &v, PETSC_FALSE);CHKERRQ(ierr);
+      /*
+         Compute the relative error associated to each eigenpair
+      */
+      ierr = EPSComputeRelativeError(eps,i,&error);CHKERRQ(ierr);
+
+#ifdef PETSC_USE_COMPLEX
+      re = PetscRealPart(kr);
+      im = PetscImaginaryPart(kr);
+#else
+      re = kr;
+      im = ki;
+#endif 
+      if (im!=0.0) {
+        ierr = PetscPrintf(PETSC_COMM_WORLD," %9f%+9f j %12f\n",re,im,error);CHKERRQ(ierr);
+      } else {
+        ierr = PetscPrintf(PETSC_COMM_WORLD,"   %12f       %12f\n",re,error);CHKERRQ(ierr); 
+      }
+    }
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"\n" );CHKERRQ(ierr);
+  }
+  ierr = VecDestroy(v);CHKERRQ(ierr);
+  
+  ierr = EPSSolve(eps);CHKERRQ(ierr);
+  ierr = EPSGetConverged(eps,&nconv);CHKERRQ(ierr);
+  ierr = EPSGetIterationNumber(eps, &its);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of iterations of the method: %d\n",its);
+         CHKERRQ(ierr);
+  /*
+     Optional: Get some information from the solver and display it
+  */
+  ierr = EPSGetType(eps,&type);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Solution method: %s\n\n",type);CHKERRQ(ierr);
+  ierr = EPSGetDimensions(eps,&nev,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of requested eigenvalues: %d\n",nev);
+         CHKERRQ(ierr);
+  ierr = EPSGetTolerances(eps,&tol,&maxit);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Stopping condition: tol=%.4g, maxit=%d\n",tol,maxit);
+         CHKERRQ(ierr);
+
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                    Display solution and clean up
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* 
+     Get number of converged approximate eigenpairs
+  */
+  ierr = EPSGetConverged(eps,&nconv);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of converged eigenpairs: %d\n\n",nconv);
+         CHKERRQ(ierr);
+
+  if (nconv>0) {
+    /*
+       Display eigenvalues and relative errors
+    */
+    ierr = PetscPrintf(PETSC_COMM_WORLD,
+         "           k          ||Ax-kx||/||kx||\n"
+         "   ----------------- ------------------\n" );CHKERRQ(ierr);
+
+    for( i=0; i<nconv; i++ ) {
+      /* 
+        Get converged eigenpairs: i-th eigenvalue is stored in kr (real part) and
+        ki (imaginary part)
+      */
+      ierr = EPSGetEigenpair(eps,i,&kr,&ki,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
+      /*
+         Compute the relative error associated to each eigenpair
+      */
+      ierr = EPSComputeRelativeError(eps,i,&error);CHKERRQ(ierr);
+
+#ifdef PETSC_USE_COMPLEX
+      re = PetscRealPart(kr);
+      im = PetscImaginaryPart(kr);
+#else
+      re = kr;
+      im = ki;
+#endif 
+      if (im!=0.0) {
+        ierr = PetscPrintf(PETSC_COMM_WORLD," %9f%+9f j %12f\n",re,im,error);CHKERRQ(ierr);
+      } else {
+        ierr = PetscPrintf(PETSC_COMM_WORLD,"   %12f       %12f\n",re,error);CHKERRQ(ierr); 
+      }
+    }
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"\n" );CHKERRQ(ierr);
+  }
+  
+  /* 
+     Free work space
+  */
+  ierr = EPSDestroy(eps);CHKERRQ(ierr);
+  ierr = MatDestroy(A);CHKERRQ(ierr);
+  ierr = SlepcFinalize();CHKERRQ(ierr);
+  return 0;
+}
+
