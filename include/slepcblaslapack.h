4c9564a4c0d372989ef7614b6dbe3cc59ca92f68
Andres Tomas <antodo@upvnet.upv.es>
2004-06-22 09:26:47 +0000

    Cleanup of function declarations.
    (Logical change 1.178)
    
    
    Subversion-commit: r460

--- include/slepcblaslapack.h
+++ include/slepcblaslapack.h
@@ -1,4 +1,5 @@
 /*
+
   Necessary routines in BLAS and LAPACK not included in petscblaslapack.f
 
 */
@@ -17,119 +18,125 @@
 /*
    For these machines we must call the single precision Fortran version
 */
-#define DLARNV   SLARNV
-#define DLAPY2   SLAPY2
-#define DLAEV2   SLAEV2
-#define DGELQF   SGELQF
-#define DORMLQ   SORMLQ
-#define DHSEQR   SHSEQR
-#define DTREVC   STREVC
-#define DGEHRD   SGEHRD
-#define DORGHR   SORGHR
-#define DGEES    SGEES 
+#define LAlapy2_ SLAPY2
+#define LAlaev2_ SLAEV2
+#define LAgehrd_ SGEHRD
+#define LAorghr_ SORGHR
+#define LAlanhs_ SLANHS
+#define LAlange_ SLANGE
+#define LAgetri_ SGETRI
+#define LAhseqr_ SHSEQR
+#define LAtrexc_ STREXC
+#define LAtrevc_ STREVC
 #endif
 
 #if defined(PETSC_HAVE_FORTRAN_UNDERSCORE) || defined(PETSC_BLASLAPACK_F2C)
-#define LArnv_   dlarnv_
 #define LAlapy2_ dlapy2_
 #define LAlaev2_ dlaev2_
-#define LAgelqf_ dgelqf_
-#define LAormlq_ dormlq_
-#define LAhseqr_ dhseqr_
-#define LAtrevc_ dtrevc_
 #define LAgehrd_ dgehrd_
 #define LAorghr_ dorghr_
-#define LAgees_  dgees_
+#define LAlanhs_ dlanhs_
+#define LAlange_ dlange_
+#define LAgetri_ dgetri_
+#define LAhseqr_ dhseqr_
+#define LAtrexc_ dtrexc_
+#define LAtrevc_ dtrevc_
 #elif defined(PETSC_HAVE_FORTRAN_CAPS)
-#define LArnv_   DLARNV
 #define LAlapy2_ DLAPY2
 #define LAlaev2_ DLAEV2
-#define LAgelqf_ DGELQF
-#define LAormlq_ DORMLQ
-#define LAhseqr_ DHSEQR
-#define LAtrevc_ DTREVC
 #define LAgehrd_ DGEHRD
 #define LAorghr_ DORGHR
-#define LAgees_  DGEES 
+#define LAlanhs_ DLANHS
+#define LAlange_ DLANGE
+#define LAgetri_ DGETRI
+#define LAhseqr_ DHSEQR
+#define LAtrexc_ DTREXC
+#define LAtrevc_ DTREVC
 #else
-#define LArnv_   dlarnv
 #define LAlapy2_ dlapy2
 #define LAlaev2_ dlaev2
-#define LAgelqf_ dgelqf
-#define LAormlq_ dormlq
-#define LAhseqr_ dhseqr
-#define LAtrevc_ dtrevc
 #define LAgehrd_ dgehrd
 #define LAorghr_ dorghr
-#define LAgees_  dgees
+#define LAlanhs_ dlanhs
+#define LAlange_ dlange
+#define LAgetri_ dgetri
+#define LAhseqr_ dhseqr
+#define LAtrexc_ dtrexc
+#define LAtrevc_ dtrevc
 #endif
 
 #else
 /*
-   Complex with no character string arguments
+  Complex
 */
 #if defined(PETSC_USES_FORTRAN_SINGLE)
-#define ZLARNV   CLARNV
-#define ZGELQF   CGELQF
-#define ZUNMLQ   CUNMLQ
-#define ZHSEQR   CHSEQR
-#define ZTREVC   CTREVC
+#define DLAPY2   DLAPY2
+#define ZLAEV2   CLAEV2
 #define ZGEHRD   CGEHRD
 #define ZUNGHR   CUNGHR
-#define ZGEES    CGEES 
+#define ZLANHS   CLANHS
+#define ZLANGE   CLANGE
+#define ZGETRI   CGETRI
+#define ZHSEQR   CHSEQR
+#define ZTREXC   CTREXC
+#define ZTREVC   CTREVC
 #endif
 
 #if defined(PETSC_HAVE_FORTRAN_UNDERSCORE) || defined(PETSC_BLASLAPACK_F2C)
-#define LArnv_   zlarnv_
+#define LAlapy2_ dlapy2_
 #define LAlaev2_ zlaev2_
-#define LAgelqf_ zgelqf_
-#define LAormlq_ zunmlq_
-#define LAhseqr_ zhseqr_
-#define LAtrevc_ ztrevc_
 #define LAgehrd_ zgehrd_
 #define LAorghr_ zunghr_
-#define LAgees_  zgees_ 
+#define LAlanhs_ zlanhs_
+#define LAlange_ zlange_
+#define LAgetri_ zgetri_
+#define LAhseqr_ zhseqr_
+#define LAtrexc_ ztrexc_
+#define LAtrevc_ ztrevc_
 #elif defined(PETSC_HAVE_FORTRAN_CAPS)
-#define LArnv_   ZLARNV
+#define LAlapy2_ DLAPY2
 #define LAlaev2_ ZLAEV2
-#define LAgelqf_ ZGELQF
-#define LAormlq_ ZUNMLQ
-#define LAhseqr_ ZHSEQR
-#define LAtrevc_ ZTREVC
 #define LAgehrd_ ZGEHRD
 #define LAorghr_ ZUNGHR
-#define LAgees_  ZGEES 
+#define LAlanhs_ ZLANHS
+#define LAlange_ ZLANGE
+#define LAgetri_ ZGETRI
+#define LAhseqr_ ZHSEQR
+#define LAtrexc_ ZTREXC
+#define LAtrevc_ ZTREVC
 #else
-#define LArnv_   zlarnv
+#define LAlapy2_ dlapy2
 #define LAlaev2_ zlaev2
-#define LAgelqf_ zgelqf
-#define LAormlq_ zunmlq
-#define LAhseqr_ zhseqr
-#define LAtrevc_ ztrevc
 #define LAgehrd_ zgehrd
 #define LAorghr_ zunghr
-#define LAgees_  zgees 
+#define LAlanhs_ zlanhs
+#define LAlange_ zlange
+#define LAgetri_ zgetri
+#define LAhseqr_ zhseqr
+#define LAtrexc_ ztrexc
+#define LAtrevc_ ztrevc
 #endif
 
 #endif
 
 EXTERN_C_BEGIN
 
-extern void   LArnv_(int*,int*,int*,PetscScalar*);
-extern double LAlapy2_(double*,double*);
-extern void   LAlaev2_(PetscScalar*,PetscScalar*,PetscScalar*,PetscReal*,PetscReal*,PetscReal*,PetscScalar*);
-extern void   LAgelqf_(int*,int*,PetscScalar*,int*,PetscScalar*,PetscScalar*,int*,int*);
-extern void   LAormlq_(char*,char*,int*,int*,int*,PetscScalar*,int*,PetscScalar*,PetscScalar*,int*,PetscScalar*,int*,int*,int,int);
-extern void   LAgehrd_(int*,int*,int*,PetscScalar*,int*,PetscScalar*,PetscScalar*,int*,int*);
-extern void   LAorghr_(int*,int*,int*,PetscScalar*,int*,PetscScalar*,PetscScalar*,int*,int*);
+EXTERN PetscReal LAlapy2_(PetscReal*,PetscReal*);
+EXTERN void      LAlaev2_(PetscScalar*,PetscScalar*,PetscScalar*,PetscReal*,PetscReal*,PetscReal*,PetscScalar*);
+EXTERN void      LAgehrd_(int*,int*,int*,PetscScalar*,int*,PetscScalar*,PetscScalar*,int*,int*);
+EXTERN void      LAorghr_(int*,int*,int*,PetscScalar*,int*,PetscScalar*,PetscScalar*,int*,int*);
+EXTERN PetscReal LAlanhs_(const char*,int*,PetscScalar*,int*,PetscReal*,int);
+EXTERN PetscReal LAlange_(const char*,int*,int*,PetscScalar*,int*,PetscReal*,int);
+EXTERN void      LAgetri_(int*,PetscScalar*,int*,int*,PetscScalar*,int*,int*);
+
 #if !defined(PETSC_USE_COMPLEX)
-extern void   LAhseqr_(char*,char*,int*,int*,int*,PetscScalar*,int*,PetscScalar*,PetscScalar*,PetscScalar*,int*,PetscScalar*,int*,int*,int,int);
-extern void   LAgees_(char*,char*,int*,int*,PetscScalar*,int*,int*,PetscScalar*,PetscScalar*,PetscScalar*,int*,PetscScalar*,int*,int*,int*);
-extern void   LAtrevc_(char*,char*,int*,int*,PetscScalar*,int*,PetscScalar*,int*,PetscScalar*,int*,int*,int*,PetscScalar*,int*,int,int);
+EXTERN void      LAhseqr_(const char*,const char*,int*,int*,int*,PetscScalar*,int*,PetscScalar*,PetscScalar*,PetscScalar*,int*,PetscScalar*,int*,int*,int,int);
+EXTERN void      LAtrexc_(const char*,int*,PetscScalar*,int*,PetscScalar*,int*,int*,int*,PetscScalar*,int*,int);
+EXTERN void      LAtrevc_(const char*,const char*,int*,int*,PetscScalar*,int*,PetscScalar*,int*,PetscScalar*,int*,int*,int*,PetscScalar*,int*,int,int);
 #else
-extern void   LAhseqr_(char*,char*,int*,int*,int*,PetscScalar*,int*,PetscScalar*,PetscScalar*,int*,PetscScalar*,int*,int*,int,int);
-extern void   LAgees_(char*,char*,int*,int*,PetscScalar*,int*,int*,PetscScalar*,PetscScalar*,int*,PetscScalar*,int*,PetscReal*,int*,int*);
-extern void   LAtrevc_(char*,char*,int*,int*,PetscScalar*,int*,PetscScalar*,int*,PetscScalar*,int*,int*,int*,PetscScalar*,PetscReal*,int*,int,int);
+EXTERN void      LAhseqr_(const char*,const char*,int*,int*,int*,PetscScalar*,int*,PetscScalar*,PetscScalar*,int*,PetscScalar*,int*,int*,int,int);
+EXTERN void      LAtrexc_(const char*,int*,PetscScalar*,int*,PetscScalar*,int*,int*,int*,int*,int);
+EXTERN void      LAtrevc_(const char*,const char*,int*,int*,PetscScalar*,int*,PetscScalar*,int*,PetscScalar*,int*,int*,int*,PetscScalar*,PetscReal*,int*,int,int);
 #endif
 
 EXTERN_C_END
