f200145e873e50f1d29a85bc9bcf186940b18efc
Jose E. Roman <jroman@dsic.upv.es>
2004-07-07 11:34:45 +0000

    Documentation of arnoldi - including some fixes
    (Logical change 1.196)
    
    
    Subversion-commit: r538

--- src/eps/impls/arnoldi/arnoldi.c
+++ src/eps/impls/arnoldi/arnoldi.c
@@ -1,7 +1,32 @@
-
 /*                       
-       This implements the Arnoldi method with explicit restart and
-       deflation.
+
+   SLEPc eigensolver: "arnoldi"
+
+   Method: Explicitly Restarted Arnoldi
+
+   Description:
+
+       This solver implements the Arnoldi method with explicit restart
+       and deflation.
+
+   Algorithm:
+
+       The implemented algorithm builds an Arnoldi factorization of order
+       ncv. Converged eigenpairs are locked and the iteration is restarted
+       with the rest of the columns being the active columns for the next
+       Arnoldi factorization. Currently, no filtering is applied to the
+       vector used for restarting.
+
+   References:
+
+       [1] Z. Bai et al. (eds.), "Templates for the Solution of Algebraic
+       Eigenvalue Problems: A Practical Guide", SIAM (2000), pp 161-165.
+
+       [2] Y. Saad, "Numerical Methods for Large Eigenvalue Problems",
+       John Wiley (1992), pp 172-183.
+
+   Last update: June 2004
+
 */
 #include "src/eps/epsimpl.h"
 #include "slepcblaslapack.h"
@@ -32,6 +57,18 @@ PetscErrorCode EPSSetUp_ARNOLDI(EPS eps)
 
 #undef __FUNCT__  
 #define __FUNCT__ "EPSBasicArnoldi"
+/*
+   EPSBasicArnoldi - Computes an m-step Arnoldi factorization. The first k
+   columns are assumed to be locked and therefore they are not modified. On
+   exit, the following relation is satisfied:
+
+                    OP * V - V * H = f * e_m^T
+
+   where the columns of V are the Arnoldi vectors (which are B-orthonormal),
+   H is an upper Hessenberg matrix, f is the next vector (v_{m+1}) and e_m
+   is the m-th vector of the canonical basis. On exit, beta contains the
+   B-norm of f.
+*/
 static PetscErrorCode EPSBasicArnoldi(EPS eps,PetscScalar *H,Vec *V,int k,int m,Vec f,PetscReal *beta)
 {
   PetscErrorCode ierr;
@@ -42,16 +79,15 @@ static PetscErrorCode EPSBasicArnoldi(EPS eps,PetscScalar *H,Vec *V,int k,int m,
 
   PetscFunctionBegin;
   for (j=k;j<m-1;j++) {
-    ierr = STApply(eps->OP,V[j],f);CHKERRQ(ierr);
-    ierr = EPSOrthogonalize(eps,j+1,V,f,H+m*j,&norm,&breakdown);CHKERRQ(ierr);
+    ierr = STApply(eps->OP,V[j],V[j+1]);CHKERRQ(ierr);
+    ierr = EPSOrthogonalize(eps,j+1,V,V[j+1],H+m*j,&norm,&breakdown);CHKERRQ(ierr);
     if (breakdown) SETERRQ(1,"Breakdown in Arnoldi method");
     H[(m+1)*j+1] = norm;
     t = 1 / norm;
-    ierr = VecScale(&t,f);CHKERRQ(ierr);
-    ierr = VecCopy(f,V[j+1]);CHKERRQ(ierr);
+    ierr = VecScale(&t,V[j+1]);CHKERRQ(ierr);
   }
-  ierr = STApply(eps->OP,V[j],f);CHKERRQ(ierr);
-  ierr = EPSOrthogonalize(eps,j+1,V,f,H+m*j,beta,&breakdown);CHKERRQ(ierr);
+  ierr = STApply(eps->OP,V[m-1],f);CHKERRQ(ierr);
+  ierr = EPSOrthogonalize(eps,m,V,f,H+m*(m-1),beta,&breakdown);CHKERRQ(ierr);
   if (breakdown) SETERRQ(1,"Breakdown in Arnoldi method");
   t = 1 / *beta;
   ierr = VecScale(&t,f);CHKERRQ(ierr);
@@ -83,35 +119,57 @@ PetscErrorCode EPSSolve_ARNOLDI(EPS eps)
   ierr = PetscMalloc(ncv*sizeof(PetscReal),&rwork);CHKERRQ(ierr);
 #endif
 
+  /* The first Arnoldi vector is the normalized initial vector */
   ierr = VecCopy(eps->vec_initial,eps->V[0]);CHKERRQ(ierr);
-  ierr = VecNorm(eps->V[0],NORM_2,&norm);CHKERRQ(ierr);
+  ierr = STNorm(eps->OP,eps->V[0],&norm);CHKERRQ(ierr);
   t = 1 / norm;
   ierr = VecScale(&t,eps->V[0]);CHKERRQ(ierr);
   
   eps->nconv = 0;
   eps->its = 0;
+
+  /* Restart loop */
   while (eps->its<eps->max_it) {
+
     eps->its = eps->its + 1;
-  /* [H,V,f,beta] = karnoldi(es,H,V,nconv+1,m) % Arnoldi factorization */
+
+    /* Compute an ncv-step Arnoldi factorization */
     ierr = EPSBasicArnoldi(eps,H,eps->V,eps->nconv,ncv,f,&beta);CHKERRQ(ierr);
-  /* U = eye(m,m) */
+
+    /* At this point, H has the following structure
+
+              | *   * | *   *   *   * |
+              |     * | *   *   *   * |
+              | ------|-------------- |
+          H = |       | *   *   *   * |
+              |       | *   *   *   * |
+              |       |     *   *   * |
+              |       |         *   * |
+
+       that is, an upper Hessenberg matrix of order ncv whose principal 
+       submatrix of order nconv is (quasi-)triangular.  */
+
+    /* Reduce H to (quasi-)triangular form, H <- U H U' */
     ierr = PetscMemzero(U,ncv*ncv*sizeof(PetscScalar));CHKERRQ(ierr);
     for (i=0;i<ncv;i++) { U[i*(ncv+1)] = 1.0; }
-  /* [T,wr0,wi0,U] = laqr3(H,U,nconv+1,ncv) */
     ierr = EPSDenseSchur(H,U,eps->eigr,eps->eigi,eps->nconv,ncv);CHKERRQ(ierr);
+
+    /* Compute eigenvectors Y of H */
     ierr = PetscMemcpy(Y,U,ncv*ncv*sizeof(PetscScalar));CHKERRQ(ierr);
-  /* [Y,dummy] = eig(H) */
 #if !defined(PETSC_USE_COMPLEX)
     LAtrevc_("R","B",PETSC_NULL,&ncv,H,&ncv,PETSC_NULL,&ncv,Y,&ncv,&ncv,&mout,work,&info,1,1);
 #else
     LAtrevc_("R","B",PETSC_NULL,&ncv,H,&ncv,PETSC_NULL,&ncv,Y,&ncv,&ncv,&mout,work,rwork,&info,1,1);
 #endif
     if (info) SETERRQ1(PETSC_ERR_LIB,"Error in Lapack xTREVC %i",info);
-  /* rsd = beta*abs(Y(m,:)) */
+
+    /* Compute residual norm estimates as beta*abs(Y(m,:)) */
     for (i=eps->nconv;i<ncv;i++) { 
       eps->errest[i] = beta*PetscAbsScalar(Y[i*ncv+ncv-1]); 
     }  
-  /* check convergence */
+
+    /* Look for converged eigenpairs. If necessary, reorder the Arnoldi 
+       factorization so that all converged eigenvalues are first */
     k = eps->nconv;
     while (k<ncv && eps->errest[k]<eps->tol) k++;
     for (i=k+1;i<ncv;i++) {
@@ -130,10 +188,11 @@ PetscErrorCode EPSSolve_ARNOLDI(EPS eps)
       if (eps->eigi[i] != 0) i++;
 #endif
     }
-  /* sort eigenvalues */
+
+    /* Sort the remaining columns of the Schur form  */
     ierr = EPSSortDenseSchur(H,U,eps->eigr,eps->eigi,k,ncv);CHKERRQ(ierr);
 
-  /* V(:,idx) = V*U(:,idx) */
+    /* Update V(:,idx) = V*U(:,idx) */
     ierr = EPSReverseProjection(eps,eps->V,U,eps->nconv,ncv,eps->work);CHKERRQ(ierr);
     eps->nconv = k;
     EPSMonitor(eps,eps->its,eps->nconv,eps->eigr,eps->eigi,eps->errest,ncv);
