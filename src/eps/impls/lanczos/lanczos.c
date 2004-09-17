e1de5ef055480bcf03ef13f91ce493b8e451de06
Jose E. Roman <jroman@dsic.upv.es>
2004-09-17 16:49:51 +0000

    Cosmetic changes in Lanczos
    (Logical change 1.242)
    
    
    Subversion-commit: r655

--- src/eps/impls/lanczos/lanczos.c
+++ src/eps/impls/lanczos/lanczos.c
@@ -2,15 +2,44 @@
 
    SLEPc eigensolver: "lanczos"
 
-   Method: 
+   Method: Explicitly Restarted Symmetric/Hermitian Lanczos
 
    Description:
 
+       This solver implements the Lanczos method for symmetric (Hermitian)
+       problems, with explicit restart and deflation. When building the 
+       Lanczos factorization, several reorthogonalization strategies can
+       be selected.
+
    Algorithm:
+
+       The implemented algorithm builds a Lanczos factorization of order
+       ncv. Converged eigenpairs are locked and the iteration is restarted
+       with the rest of the columns being the active columns for the next
+       Lanczos factorization. Currently, no filtering is applied to the
+       vector used for restarting.
+
+       The following reorthogonalization schemes are currently implemented:
+
+       - Full reorthogonalization: at each Lanczos step, the corresponding
+       Lanczos vector is orthogonalized with respect to all the previous
+       vectors.
 
    References:
 
-   Last update: 
+       [1] B.N. Parlett, "The Symmetric Eigenvalue Problem", SIAM Classics in 
+       Applied Mathematics (1998), ch. 13.
+
+       [2] L. Komzsik, "The Lanczos Method. Evolution and Application", SIAM
+       (2003).
+
+       [3] B.N. Parlett and D.S. Scott, The Lanczos algorithm with selective
+       orthogonalization, Math. Comp., 33 (1979), no. 145, 217-238.
+
+       [4] H.D. Simon, The Lanczos algorithm with partial reorthogonalization,
+       Math. Comp., 42 (1984), no. 165, 115-142.
+
+   Last update: October 2004
 
 */
 #include "src/eps/epsimpl.h"
@@ -44,12 +73,14 @@ PetscErrorCode EPSSetUp_LANCZOS(EPS eps)
 }
 
 #undef __FUNCT__  
-#define __FUNCT__ "EPSBasicLanczos"
+#define __FUNCT__ "EPSFullLanczos"
 /*
-   EPSBasicLanczos - 
-                    OP * V - V * H = f * e_m^T
+   EPSFullLanczos - Full reorthogonalization.
+
+   In this variant, at each Lanczos step, the corresponding Lanczos vector 
+   is orthogonalized with respect to all the previous Lanczos vectors.
 */
-static PetscErrorCode EPSBasicLanczos(EPS eps,PetscScalar *H,Vec *V,int k,int m,Vec f,PetscReal *beta)
+static PetscErrorCode EPSFullLanczos(EPS eps,PetscScalar *T,Vec *V,int k,int m,Vec f,PetscReal *beta)
 {
   PetscErrorCode ierr;
   int            j;
@@ -61,10 +92,10 @@ static PetscErrorCode EPSBasicLanczos(EPS eps,PetscScalar *H,Vec *V,int k,int m,
   for (j=k;j<m-1;j++) {
     ierr = STApply(eps->OP,V[j],V[j+1]);CHKERRQ(ierr);
     ierr = EPSOrthogonalize(eps,eps->nds,eps->DS,V[j+1],PETSC_NULL,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
-    ierr = EPSOrthogonalize(eps,j+1,V,V[j+1],H+m*j,&norm,&breakdown);CHKERRQ(ierr);
-    H[(m+1)*j+1] = norm;
+    ierr = EPSOrthogonalize(eps,j+1,V,V[j+1],T+m*j,&norm,&breakdown);CHKERRQ(ierr);
+    T[(m+1)*j+1] = norm;
     if (breakdown) {
-      PetscLogInfo(eps,"Breakdown in Arnoldi method (norm=%g)\n",norm);
+      PetscLogInfo(eps,"Breakdown in Lanczos method (norm=%g)\n",norm);
       ierr = SlepcVecSetRandom(V[j+1]);CHKERRQ(ierr);
       ierr = STNorm(eps->OP,V[j+1],&norm);CHKERRQ(ierr);
     }
@@ -72,10 +103,37 @@ static PetscErrorCode EPSBasicLanczos(EPS eps,PetscScalar *H,Vec *V,int k,int m,
     ierr = VecScale(&t,V[j+1]);CHKERRQ(ierr);
   }
   ierr = STApply(eps->OP,V[m-1],f);CHKERRQ(ierr);
-  ierr = EPSOrthogonalize(eps,m,V,f,H+m*(m-1),beta,PETSC_NULL);CHKERRQ(ierr);
+  ierr = EPSOrthogonalize(eps,m,V,f,T+m*(m-1),beta,PETSC_NULL);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
+#undef __FUNCT__  
+#define __FUNCT__ "EPSBasicLanczos"
+/*
+   EPSBasicLanczos - Computes an m-step Lanczos factorization. The first k
+   columns are assumed to be locked and therefore they are not modified. On
+   exit, the following relation is satisfied:
+
+                    OP * V - V * T = f * e_m^T
+
+   where the columns of V are the Lanczos vectors, T is a tridiagonal matrix, 
+   f is the residual vector and e_m is the m-th vector of the canonical basis. 
+   The Lanczos vectors (together with vector f) are B-orthogonal (to working
+   accuracy) if full reorthogonalization is being used, otherwise they are
+   (B-)semi-orthogonal. On exit, beta contains the B-norm of f and the next 
+   Lanczos vector can be computed as v_{m+1} = f / beta. 
+
+   This function simply calls another function which depends on the selected
+   reorthogonalization strategy.
+*/
+static PetscErrorCode EPSBasicLanczos(EPS eps,PetscScalar *T,Vec *V,int k,int m,Vec f,PetscReal *beta)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = EPSFullLanczos(eps,T,V,k,m,f,beta);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
 
 #undef __FUNCT__  
 #define __FUNCT__ "EPSSolve_LANCZOS"
@@ -88,11 +146,11 @@ PetscErrorCode EPSSolve_LANCZOS(EPS eps)
                  liwork=10*ncv;
   Vec            f=eps->work[ncv];
   PetscScalar    *T=eps->T,*Y,ts;
-  PetscReal      *D,*E,*W,*work,norm,beta,abstol=0;
+  PetscReal      *D,*E,*W,*work,norm,beta,abstol;
 
   PetscFunctionBegin;
 #if defined(PETSC_BLASLAPACK_ESSL_ONLY)
-  SETERRQ(PETSC_ERR_SUP,"xSTEVX - Lapack routine is unavailable.");
+  SETERRQ(PETSC_ERR_SUP,"xSTEGR - Lapack routine is unavailable.");
 #endif 
   ierr = PetscMemzero(T,ncv*ncv*sizeof(PetscScalar));CHKERRQ(ierr);
   ierr = PetscMalloc(ncv*ncv*sizeof(PetscScalar),&Y);CHKERRQ(ierr);
@@ -100,27 +158,48 @@ PetscErrorCode EPSSolve_LANCZOS(EPS eps)
   ierr = PetscMalloc(ncv*sizeof(PetscReal),&E);CHKERRQ(ierr);
   ierr = PetscMalloc(ncv*sizeof(PetscReal),&W);CHKERRQ(ierr);
   ierr = PetscMalloc(2*ncv*sizeof(int),&isuppz);CHKERRQ(ierr);
-  ierr = PetscMalloc(lwork*sizeof(PetscScalar),&work);CHKERRQ(ierr);
+  ierr = PetscMalloc(lwork*sizeof(PetscReal),&work);CHKERRQ(ierr);
   ierr = PetscMalloc(liwork*sizeof(int),&iwork);CHKERRQ(ierr);
 
-  /* The first vector is the normalized initial vector */
+  /* The first Lanczos vector is the normalized initial vector */
   ierr = VecCopy(eps->vec_initial,eps->V[0]);CHKERRQ(ierr);
   ierr = STNorm(eps->OP,eps->V[0],&norm);CHKERRQ(ierr);
   ts = 1 / norm;
   ierr = VecScale(&ts,eps->V[0]);CHKERRQ(ierr);
   
-  eps->nconv = nconv = 0;
+  nconv = 0;
   eps->its = 0;
+  /* Restart loop */
   while (eps->its<eps->max_it) {
+    /* Compute an ncv-step Lanczos factorization */
     ierr = EPSBasicLanczos(eps,T,eps->V,nconv,ncv,f,&beta);CHKERRQ(ierr);
 
+    /* At this point, T has the following structure
+
+              | *     |               |
+              |     * |               |
+              | ------|-------------- |
+          T = |       | *   *         |
+              |       | *   *   *     |
+              |       |     *   *   * |
+              |       |         *   * |
+
+       that is, a real symmetric tridiagonal matrix of order ncv whose 
+       principal submatrix of order nconv is diagonal.  */
+
+    /* Extract the tridiagonal block from T. Store the diagonal elements in D 
+       and the off-diagonal elements in E  */
     n = ncv - nconv;
     for (i=0;i<n;i++) {
       D[i] = PetscRealPart(T[(i+nconv)*(ncv+1)]);
       E[i] = PetscRealPart(T[(i+nconv)*(ncv+1)+1]);
     }
-    LAstegr_("V","A",&n,D,E,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,&abstol,&m,W,Y,&n,isuppz,work,&lwork,iwork,&liwork,&info,1,1);
-    if (info) SETERRQ1(PETSC_ERR_LIB,"Error in Lapack xSTEVX %i",info);
+
+    /* Compute eigenvalues W and eigenvectors Y of the tridiagonal block */
+    abstol = 0.0;
+    LAstegr_("V","A",&n,D,E,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,
+             &abstol,&m,W,Y,&n,isuppz,work,&lwork,iwork,&liwork,&info,1,1);
+    if (info) SETERRQ1(PETSC_ERR_LIB,"Error in Lapack xSTEGR %i",info);
     
     /* Compute residual norm estimates as beta*abs(Y(m,:)) */
     for (i=0;i<n;i++) {
@@ -128,7 +207,7 @@ PetscErrorCode EPSSolve_LANCZOS(EPS eps)
       eps->errest[ncv-i-1] = beta*PetscAbsScalar(Y[i*n+n-1]);
     }
     
-    /* Update V(:,idx) = V*U(:,idx) */
+    /* Update V(:,idx) = Y*U(:,idx) */
     for (i=0;i<n;i++) 
       for (j=0;j<n;j++) 
           T[i+nconv+(j+nconv)*ncv] = Y[i+(n-j-1)*n];
@@ -138,11 +217,11 @@ PetscErrorCode EPSSolve_LANCZOS(EPS eps)
     while (nconv<ncv && eps->errest[nconv]<eps->tol) nconv++;
 
     EPSMonitor(eps,eps->its,nconv,eps->eigr,eps->eigi,eps->errest,ncv);
-    eps->its = eps->its + ncv - eps->nconv;
-    eps->nconv = nconv;
-    if (eps->nconv >= eps->nev) break;
+    eps->its = eps->its + ncv - nconv;
+    if (nconv >= eps->nev) break;
   }
   
+  eps->nconv = nconv;
   if( eps->nconv >= eps->nev ) eps->reason = EPS_CONVERGED_TOL;
   else eps->reason = EPS_DIVERGED_ITS;
   for (i=0;i<eps->nconv;i++) eps->eigi[i]=0.0;
