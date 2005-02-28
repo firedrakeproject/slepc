9900c97cda5d0c453039ac9a31cd345b74601807
Andres Tomas <antodo@upvnet.upv.es>
2005-02-28 15:55:20 +0000

    (Logical change 1.285)
    Subversion-commit: r778

---
+++ src/st/impls/fold/fold.c
@@ -0,0 +1,233 @@
+/*
+    Folding spectral transformation, applies (A + sigma I)² as operator, or 
+    inv(B)(A + sigma I)² for generalized problems
+*/
+#include "src/st/stimpl.h"          /*I "slepcst.h" I*/
+
+typedef struct {
+  PetscTruth  left;
+  Vec         w2;
+} ST_FOLD;
+
+#undef __FUNCT__  
+#define __FUNCT__ "STApply_Fold"
+PetscErrorCode STApply_Fold(ST st,Vec x,Vec y)
+{
+  PetscErrorCode ierr;
+  PetscScalar    alpha;
+  ST_FOLD        *ctx = (ST_FOLD *) st->data;
+
+  PetscFunctionBegin;
+  if (st->B) {
+    /* generalized eigenproblem: y = (B^-1 A + sI)² x */
+    if (st->sigma != 0.0) {
+      alpha = - st->sigma;
+      ierr = MatMult(st->A,x,st->w);CHKERRQ(ierr);
+      ierr = STAssociatedKSPSolve(st,st->w,ctx->w2);CHKERRQ(ierr);
+      ierr = VecAXPY(&alpha,x,ctx->w2);CHKERRQ(ierr);
+      ierr = MatMult(st->A,ctx->w2,st->w);CHKERRQ(ierr);
+      ierr = STAssociatedKSPSolve(st,st->w,y);CHKERRQ(ierr);
+      ierr = VecAXPY(&alpha,ctx->w2,y);CHKERRQ(ierr);
+    } else {
+      ierr = MatMult(st->A,x,st->w);CHKERRQ(ierr);
+      ierr = STAssociatedKSPSolve(st,st->w,y);CHKERRQ(ierr);
+      ierr = MatMult(st->A,y,st->w);CHKERRQ(ierr);
+      ierr = STAssociatedKSPSolve(st,st->w,y);CHKERRQ(ierr);
+    }
+  } else {
+    /* standard eigenproblem: y = (A + sI)² x */
+    ierr = MatMult(st->A,x,st->w);CHKERRQ(ierr);
+    alpha = - st->sigma;
+    if (st->sigma != 0.0) {
+      ierr = VecAXPY(&alpha,x,st->w);CHKERRQ(ierr);
+    }
+    ierr = MatMult(st->A,st->w,y);CHKERRQ(ierr);
+    if (st->sigma != 0.0) {
+      ierr = VecAXPY(&alpha,st->w,y);CHKERRQ(ierr);
+    }
+  }
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__  
+#define __FUNCT__ "STApplyB_Fold"
+PetscErrorCode STApplyB_Fold(ST st,Vec x,Vec y)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = VecCopy( x, y ); CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__  
+#define __FUNCT__ "STBackTransform_Fold"
+PetscErrorCode STBackTransform_Fold(ST st,PetscScalar *eigr,PetscScalar *eigi)
+{
+  ST_FOLD *ctx = (ST_FOLD *) st->data;
+  PetscFunctionBegin;
+  PetscValidScalarPointer(eigr,2);
+  PetscValidScalarPointer(eigi,3);
+#if !defined(PETSC_USE_COMPLEX)
+  if (*eigi == 0) {
+#endif
+    if (ctx->left) *eigr = st->sigma - PetscSqrtScalar(*eigr);
+    else *eigr = st->sigma + PetscSqrtScalar(*eigr);
+#if !defined(PETSC_USE_COMPLEX)
+  } else {
+    PetscScalar r,x,y;
+    r = PetscSqrtScalar(*eigr * *eigr + *eigi * *eigi);
+    x = PetscSqrtScalar((r + *eigr) / 2);
+    y = PetscSqrtScalar((r - *eigr) / 2);
+    if (*eigi < 0) y = - y;
+    if (ctx->left) {
+      *eigr = st->sigma - x;
+      *eigi = - y;
+    } else {
+      *eigr = st->sigma + x;
+      *eigi = y;
+    }
+  }
+#endif
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__  
+#define __FUNCT__ "STSetUp_Fold"
+PetscErrorCode STSetUp_Fold(ST st)
+{
+  PetscErrorCode ierr;
+  ST_FOLD        *ctx = (ST_FOLD *) st->data;
+
+  PetscFunctionBegin;
+  if (st->B) {
+    ierr = KSPSetOperators(st->ksp,st->B,st->B,DIFFERENT_NONZERO_PATTERN);CHKERRQ(ierr);
+    ierr = KSPSetUp(st->ksp);CHKERRQ(ierr);
+    if (ctx->w2) { ierr = VecDestroy(ctx->w2);CHKERRQ(ierr); }
+    ierr = MatGetVecs(st->B,&ctx->w2,PETSC_NULL);CHKERRQ(ierr); 
+  } 
+  PetscFunctionReturn(0);
+}
+
+EXTERN_C_BEGIN
+#undef __FUNCT__  
+#define __FUNCT__ "STFoldSetLeftSide_Fold"
+PetscErrorCode STFoldSetLeftSide_Fold(ST st,PetscTruth left)
+{
+  ST_FOLD *ctx = (ST_FOLD *) st->data;
+
+  PetscFunctionBegin;
+  ctx->left = left;
+  PetscFunctionReturn(0);
+}
+EXTERN_C_END
+
+#undef __FUNCT__  
+#define __FUNCT__ "STFoldSetLeftSide"
+/*@
+   STFoldSetLeftSide - Sets a flag to compute eigenvalues on the left side of shift.
+
+   Collective on ST
+
+   Input Parameters:
++  st  - the spectral transformation context
+-  left - if true compute eigenvalues on the left side 
+
+   Options Database Key:
+.  -st_fold_leftside - Sets the value of the flag
+
+   Level: intermediate
+
+.seealso: STSetShift()
+@*/
+PetscErrorCode STFoldSetLeftSide(ST st,PetscTruth left)
+{
+  PetscErrorCode ierr, (*f)(ST,PetscTruth);
+
+  PetscFunctionBegin;
+  PetscValidHeaderSpecific(st,ST_COOKIE,1);
+  ierr = PetscObjectQueryFunction((PetscObject)st,"STFoldSetLeftSide_C",(void (**)(void))&f);CHKERRQ(ierr);
+  if (f) {
+    ierr = (*f)(st,left);CHKERRQ(ierr);
+  }
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__  
+#define __FUNCT__ "STView_Fold"
+PetscErrorCode STView_Fold(ST st,PetscViewer viewer)
+{
+  PetscErrorCode ierr;
+  ST_FOLD        *ctx = (ST_FOLD *) st->data;
+
+  PetscFunctionBegin;
+  if (ctx->left) {
+    ierr = PetscViewerASCIIPrintf(viewer,"  computing eigenvalues on left side of shift\n");CHKERRQ(ierr);
+  }  
+  if (st->B) {
+    ierr = STView_Default(st,viewer);CHKERRQ(ierr);
+  } 
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__  
+#define __FUNCT__ "STSetFromOptions_Fold"
+PetscErrorCode STSetFromOptions_Fold(ST st) 
+{
+  PetscErrorCode ierr;
+  ST_FOLD      *ctx = (ST_FOLD *) st->data;
+
+  PetscFunctionBegin;
+  ierr = PetscOptionsHead("ST Fold Options");CHKERRQ(ierr);
+  ierr = PetscOptionsLogical("-st_fold_leftside","Compute eigenvalues on left side of shift","STFoldSetLeftSide",ctx->left,&ctx->left,PETSC_NULL); CHKERRQ(ierr);
+  ierr = PetscOptionsTail();CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__  
+#define __FUNCT__ "STDestroy_Fold"
+PetscErrorCode STDestroy_Fold(ST st)
+{
+  PetscErrorCode ierr;
+  ST_FOLD        *ctx = (ST_FOLD *) st->data;
+
+  PetscFunctionBegin;
+  if (ctx->w2) { ierr = VecDestroy(ctx->w2);CHKERRQ(ierr); }
+  ierr = PetscFree(ctx);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+EXTERN_C_BEGIN
+#undef __FUNCT__  
+#define __FUNCT__ "STCreate_Fold"
+PetscErrorCode STCreate_Fold(ST st)
+{
+  PetscErrorCode ierr;
+  ST_FOLD        *ctx;
+
+  PetscFunctionBegin;
+
+  ierr = PetscNew(ST_FOLD,&ctx); CHKERRQ(ierr);
+  PetscMemzero(ctx,sizeof(ST_FOLD));
+  PetscLogObjectMemory(st,sizeof(ST_FOLD));
+  st->data		  = (void *) ctx;
+
+  st->ops->apply	  = STApply_Fold;
+  st->ops->applyB	  = STApplyB_Fold;
+  st->ops->applynoB	  = STApply_Fold;
+  st->ops->backtr	  = STBackTransform_Fold;
+  st->ops->setup	  = STSetUp_Fold;
+  st->ops->view 	  = STView_Fold;
+  st->ops->setfromoptions = STSetFromOptions_Fold;
+  st->ops->destroy	  = STDestroy_Fold;
+  st->checknullspace	  = 0;
+  
+  ctx->left            = PETSC_FALSE;
+  
+  ierr = PetscObjectComposeFunctionDynamic((PetscObject)st,"STFoldSetLeftSide_C","STFoldSetLeftSide_Fold",
+                    STFoldSetLeftSide_Fold);CHKERRQ(ierr);
+
+  PetscFunctionReturn(0);
+}
+EXTERN_C_END
+
