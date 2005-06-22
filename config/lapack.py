b090d5964654d5aa58a3fac357a2b997dd2c20cd
Andres Tomas <antodo@upvnet.upv.es>
2005-06-22 16:58:59 +0000

    Sinchronize lapack.py with slepcblaslacpack.h
    (Logical change 1.330)
    
    
    Subversion-commit: r899

--- config/lapack.py
+++ config/lapack.py
@@ -5,7 +5,7 @@ from check import *
 
 def checkLapack(conf,scalar,precision):
 
-  functions = ['laev2','gehrd','lanhs','lange','getri','hseqr','trexc','trevc','steqr','geevx','ggevx']
+  functions = ['laev2','gehrd','lanhs','lange','getri','hseqr','trexc','trevc','geevx','ggevx']
 
   if scalar == 'real':
     functions += ['orghr','syevr','sygvd']
@@ -22,6 +22,7 @@ def checkLapack(conf,scalar,precision):
   
   missing = []
   conf.write('SLEPC_MISSING_LAPACK =')
+  
   for i in functions:
     f =  '#if defined(PETSC_HAVE_FORTRAN_UNDERSCORE) || defined(PETSC_BLASLAPACK_UNDERSCORE)\n'
     f += prefix + i + '_\n'
@@ -34,5 +35,25 @@ def checkLapack(conf,scalar,precision):
     if not checkLink([f],[],[]):
       missing.append(prefix + i)
       conf.write(' -DSLEPC_MISSING_LAPACK_' + i.upper())
+
+
+  if precision == 'double':
+    functions = ['dlamch','dstevr']
+  else:
+    functions = ['slamch','sstevr']
+
+  for i in functions:
+    f =  '#if defined(PETSC_HAVE_FORTRAN_UNDERSCORE) || defined(PETSC_BLASLAPACK_UNDERSCORE)\n'
+    f += i + '_\n'
+    f += '#elif defined(PETSC_HAVE_FORTRAN_CAPS)\n'
+    f += i.upper() + '\n'
+    f += '#else\n'
+    f += i + '\n'
+    f += '#endif\n'
+   
+    if not checkLink([f],[],[]):
+      missing.append(i)
+      conf.write(' -DSLEPC_MISSING_LAPACK_' + i.upper())
+  
   conf.write('\n')
   return missing
