8bd7a0eef5226cbd2711ef6c08902af6a2fdaa46
Andres Tomas <antodo@upvnet.upv.es>
2005-06-28 17:21:50 +0000

    (Logical change 1.337)
    Subversion-commit: r924

---
+++ config/log.py
@@ -0,0 +1,24 @@
+
+def Open(filename):
+  global f
+  f = open(filename,'w')
+  return
+
+def Println(string):
+  print string
+  f.write(string)
+  f.write('\n')
+
+def Print(string):
+  print string,
+  f.write(string+' ')
+  
+def Write(string):
+  f.write(string)
+  f.write('\n')
+  
+def Exit(string):
+  Println(string)
+  f.close()
+  sys.exit(string)
+
