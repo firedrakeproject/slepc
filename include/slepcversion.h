482fef7fac26b3beb9ede8278d24c36addd8f827
Jose E. Roman <jroman@dsic.upv.es>
2005-06-30 09:44:24 +0000

    Final modifications for release
    (Logical change 1.350)
    
    
    Subversion-commit: r955

--- include/slepcversion.h
+++ include/slepcversion.h
@@ -5,12 +5,12 @@
 /* 
    Current SLEPC version number and release date
 */
-#define SLEPC_VERSION_NUMBER "SLEPc Version 2.2.1, Released Aug 21, 2004"
+#define SLEPC_VERSION_NUMBER "SLEPc Version 2.3.0, Released Jun 30, 2005"
 #define SLEPC_VERSION_MAJOR    2
-#define SLEPC_VERSION_MINOR    2
-#define SLEPC_VERSION_SUBMINOR 1
+#define SLEPC_VERSION_MINOR    3
+#define SLEPC_VERSION_SUBMINOR 0
 #define SLEPC_VERSION_PATCH    0
-#define SLEPC_VERSION_DATE     "Aug 21, 2004"
+#define SLEPC_VERSION_DATE     "Jun 30, 2005"
 #define SLEPC_AUTHOR_INFO      "The SLEPc Team\n\
 Bug reports, questions: slepc-maint@grycap.upv.es\n\
 Web page: http://www.grycap.upv.es/slepc\n"
