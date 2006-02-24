be8233d5e5f4582a02589d4ba3b93c27b112a984
Jose E. Roman <jroman@dsic.upv.es>
2006-02-24 17:42:17 +0000

    Eliminar warning
    Subversion-commit: r1069

--- src/st/interface/stfunc.c
+++ src/st/interface/stfunc.c
@@ -442,7 +442,7 @@ PetscErrorCode STView(ST st,PetscViewer viewer)
         case SAME_NONZERO_PATTERN:      str = "same nonzero pattern";break;
         case DIFFERENT_NONZERO_PATTERN: str = "different nonzero pattern";break;
         case SUBSET_NONZERO_PATTERN:    str = "subset nonzero pattern";break;
-        default:                        SETERRQ(1,"Wrong structure flag");break;
+        default:                        SETERRQ(1,"Wrong structure flag");
       }
       ierr = PetscViewerASCIIPrintf(viewer,"Matrices A and B have %s\n",str);CHKERRQ(ierr);
     }
