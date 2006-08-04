4ee0c9ad184d319c6318dc4a836e33c3596fdbc1
Andres Tomas <antodo@upvnet.upv.es>
2006-08-04 09:23:54 +0000

    corregir el bloqueo de valores complejos en KrylovSchur
    Subversion-commit: r1172

--- src/eps/impls/krylovschur/krylovschur.c
+++ src/eps/impls/krylovschur/krylovschur.c
@@ -13,7 +13,7 @@ PetscErrorCode EPSSetUp_KRYLOVSCHUR(EPS eps)
   if (eps->nev > N) eps->nev = N;
   if (eps->ncv) {
     if (eps->ncv > N) eps->ncv = N;
-    if (eps->ncv<eps->nev) SETERRQ(1,"The value of ncv must be at least nev"); 
+    if (eps->ncv<eps->nev+1) SETERRQ(1,"The value of ncv must be at least nev+1"); 
   }
   else eps->ncv = PetscMin(N,PetscMax(2*eps->nev,eps->nev+15));
   
@@ -38,7 +38,7 @@ PetscErrorCode EPSSetUp_KRYLOVSCHUR(EPS eps)
 PetscErrorCode EPSSolve_KRYLOVSCHUR(EPS eps)
 {
   PetscErrorCode ierr;
-  int            i,k,lwork,info,ilo,p=0;
+  int            i,k,lwork,info,ilo,l=0;
   Vec            u=eps->work[0];
   PetscScalar    *S=eps->T,*Q,*tau,*work,*b;
   PetscReal      beta;
@@ -58,22 +58,29 @@ PetscErrorCode EPSSolve_KRYLOVSCHUR(EPS eps)
 
   /* Get the starting Arnoldi vector */
   ierr = EPSGetStartVector(eps,0,eps->V[0],PETSC_NULL);CHKERRQ(ierr);
+  l = 0;
   
   /* Restart loop */
   while (eps->reason == EPS_CONVERGED_ITERATING) {
 
     /* Compute an nv-step Arnoldi factorization */
     eps->nv = eps->ncv;
-    ierr = EPSBasicArnoldi(eps,PETSC_FALSE,S,eps->V,eps->nconv+p,&eps->nv,u,&beta,&breakdown);CHKERRQ(ierr);
+    ierr = EPSBasicArnoldi(eps,PETSC_FALSE,S,eps->V,eps->nconv+l,&eps->nv,u,&beta,&breakdown);CHKERRQ(ierr);
     ierr = VecScale(u,1.0/beta);CHKERRQ(ierr);
 
     /* Reduce S to (quasi-)triangular form, S <- Q S Q' */
-    ilo = eps->nconv+1;
-    LAPACKgehrd_(&eps->nv,&ilo,&eps->nv,S,&eps->ncv,tau,work,&lwork,&info);
-    if (info) SETERRQ1(PETSC_ERR_LIB,"Error in Lapack xGEHRD %d",info);
-    ierr = PetscMemcpy(Q,S,eps->ncv*eps->ncv*sizeof(PetscScalar));CHKERRQ(ierr);
-    LAPACKorghr_(&eps->nv,&ilo,&eps->nv,Q,&eps->ncv,tau,work,&lwork,&info);
-    if (info) SETERRQ1(PETSC_ERR_LIB,"Error in Lapack xORGHR %d",info);
+    if (l==0) {
+      ierr = PetscMemzero(Q,eps->nv*eps->nv*sizeof(PetscScalar));CHKERRQ(ierr);
+      for (i=0;i<eps->nv;i++) 
+        Q[i*(eps->nv+1)] = 1.0;
+    } else {
+      ilo = eps->nconv+1;
+      LAPACKgehrd_(&eps->nv,&ilo,&eps->nv,S,&eps->ncv,tau,work,&lwork,&info);
+      if (info) SETERRQ1(PETSC_ERR_LIB,"Error in Lapack xGEHRD %d",info);
+      ierr = PetscMemcpy(Q,S,eps->ncv*eps->ncv*sizeof(PetscScalar));CHKERRQ(ierr);
+      LAPACKorghr_(&eps->nv,&ilo,&eps->nv,Q,&eps->ncv,tau,work,&lwork,&info);
+      if (info) SETERRQ1(PETSC_ERR_LIB,"Error in Lapack xORGHR %d",info);
+    }
     ierr = EPSDenseSchur(eps->nv,eps->nconv,S,eps->ncv,Q,eps->eigr,eps->eigi);CHKERRQ(ierr);
     
     /* Sort the remaining columns of the Schur form */
@@ -82,44 +89,56 @@ PetscErrorCode EPSSolve_KRYLOVSCHUR(EPS eps)
     /* Compute residual norm estimates */
     ierr = ArnoldiResiduals(S,eps->ncv,Q,beta,eps->nconv,eps->nv,eps->eigr,eps->eigi,eps->errest,work);CHKERRQ(ierr);
 
-    /* Lock converged eigenpairs and update the corresponding vectors,
-       V(:,idx) = V*Q(:,idx) */
+    /* Check convergence */
     k = eps->nconv;
     while (k<eps->nv && eps->errest[k]<eps->tol) k++;
-    p = PetscMax(PetscMin(eps->ncv/2,eps->nv-k-1),0);
-    for (i=eps->nconv;i<k+p;i++) {
+    if (eps->its >= eps->max_it) eps->reason = EPS_DIVERGED_ITS;
+    if (k >= eps->nev) eps->reason = EPS_CONVERGED_TOL;
+    
+    /* update l */
+    if (eps->reason != EPS_CONVERGED_ITERATING || breakdown) l = 0;
+    else {
+      l = (eps->nv-k)/2;
+#if !defined(PETSC_USE_COMPLEX)
+      if (eps->eigi[k+l-1] > 0) {
+        if (k+l<eps->nv-1) l = l+1;
+	else l = l-1;
+      }
+#endif
+    }
+    
+    /* Lock converged eigenpairs and update the corresponding vectors,
+       V(:,idx) = V*Q(:,idx) */
+    for (i=eps->nconv;i<k+l;i++) {
       ierr = VecSet(eps->AV[i],0.0);CHKERRQ(ierr);
       ierr = VecMAXPY(eps->AV[i],eps->nv,Q+eps->nv*i,eps->V);CHKERRQ(ierr);
     }
-    for (i=eps->nconv;i<k+p;i++) {
+    for (i=eps->nconv;i<k+l;i++) {
       ierr = VecCopy(eps->AV[i],eps->V[i]);CHKERRQ(ierr);
     }
     eps->nconv = k;
 
     EPSMonitor(eps,eps->its,eps->nconv,eps->eigr,eps->eigi,eps->errest,eps->nv);
-    if (breakdown) {
-      /* start a new Arnoldi factorization */
-      eps->count_breakdown++;
-      PetscInfo2(eps,"Breakdown in Krylov-Schur method (it=%i norm=%g)\n",eps->its,beta);
-      ierr = EPSGetStartVector(eps,k,eps->V[k],&breakdown);CHKERRQ(ierr);
-      p = 0; 
+    
+    if (eps->reason == EPS_CONVERGED_ITERATING) {
       if (breakdown) {
-        eps->reason = EPS_DIVERGED_BREAKDOWN;
-	PetscInfo(eps,"Unable to generate more start vectors\n");
+	/* start a new Arnoldi factorization */
+	eps->count_breakdown++;
+	PetscInfo2(eps,"Breakdown in Krylov-Schur method (it=%i norm=%g)\n",eps->its,beta);
+	ierr = EPSGetStartVector(eps,k,eps->V[k],&breakdown);CHKERRQ(ierr);
+	if (breakdown) {
+          eps->reason = EPS_DIVERGED_BREAKDOWN;
+	  PetscInfo(eps,"Unable to generate more start vectors\n");
+	}
+      } else {
+        /* update the Arnoldi-Schur decomposition */
+	for (i=k;i<k+l;i++) {
+          S[i*eps->ncv+k+l] = Q[i*eps->nv+eps->nv-1]*beta;
+	}
+	ierr = VecCopy(u,eps->V[k+l]);CHKERRQ(ierr);
       }
-    }
-    if (eps->its >= eps->max_it) eps->reason = EPS_DIVERGED_ITS;
-    if (eps->nconv >= eps->nev) eps->reason = EPS_CONVERGED_TOL;
-
-    if (eps->reason == EPS_CONVERGED_ITERATING && !breakdown) {
-      /* update the Arnoldi-Schur decomposition */
-      for (i=k;i<k+p;i++) {
-        S[i*eps->ncv+k+p] = Q[i*eps->nv+eps->nv-1]*beta;
-      }
-      ierr = VecCopy(u,eps->V[k+p]);CHKERRQ(ierr);
-    }
-       
-  }
+    }    
+  } 
   
 #if defined(PETSC_USE_COMPLEX)
   for (i=0;i<eps->nconv;i++) eps->eigi[i]=0.0;
