35d838325b4334a0a96fa4eab41c9622b4b7e885
Andres Tomas <antodo@upvnet.upv.es>
2006-12-01 08:53:13 +0000

    version preliminar de la SVD con Lanczos
    Subversion-commit: r1280

--- src/svd/impls/lanczos/gklanczos.c
+++ src/svd/impls/lanczos/gklanczos.c
@@ -56,17 +56,16 @@ PetscErrorCode cgs2(Vec v, int n, Vec *V)
 
 #undef __FUNCT__  
 #define __FUNCT__ "computeres"
-PetscErrorCode computeres(SVD svd,PetscScalar sigma,Vec u,Vec v,PetscReal *norm)
+PetscErrorCode computeres(SVD svd,PetscScalar sigma,Vec u,Vec v,PetscReal *norm1,PetscReal *norm2)
 {
   PetscErrorCode ierr;
   Vec            x,y;
-  PetscReal      norm1,norm2;
   
   PetscFunctionBegin;
   ierr = VecDuplicate(u,&x);CHKERRQ(ierr);
   ierr = MatMult(svd->A,v,x);CHKERRQ(ierr);
   ierr = VecAXPY(x,-sigma,u);CHKERRQ(ierr);
-  ierr = VecNorm(x,NORM_2,&norm1);CHKERRQ(ierr);
+  ierr = VecNorm(x,NORM_2,norm1);CHKERRQ(ierr);
 
   ierr = VecDuplicate(v,&y);CHKERRQ(ierr);
   if (svd->AT) {
@@ -75,9 +74,8 @@ PetscErrorCode computeres(SVD svd,PetscScalar sigma,Vec u,Vec v,PetscReal *norm)
     ierr = MatMultTranspose(svd->A,u,y);CHKERRQ(ierr);
   }
   ierr = VecAXPY(y,-sigma,v);CHKERRQ(ierr);
-  ierr = VecNorm(y,NORM_2,&norm2);CHKERRQ(ierr);
+  ierr = VecNorm(y,NORM_2,norm2);CHKERRQ(ierr);
 
-  *norm = norm1 + norm2;
   ierr = VecDestroy(x);CHKERRQ(ierr);
   ierr = VecDestroy(y);CHKERRQ(ierr);
   PetscFunctionReturn(0);
@@ -89,7 +87,7 @@ PetscErrorCode computeres(SVD svd,PetscScalar sigma,Vec u,Vec v,PetscReal *norm)
 PetscErrorCode SVDSolve_LANCZOS(SVD svd)
 {
   PetscErrorCode ierr;
-  PetscReal      *alpha,*beta,norm,*work;
+  PetscReal      *alpha,*beta,norm1,norm2,*work;
   PetscScalar    *Q,*PT;
   int            i,j,k,n,zero=0,info;
   Vec            *V,*U;
@@ -109,7 +107,7 @@ PetscErrorCode SVDSolve_LANCZOS(SVD svd)
   svd->nconv = 0;
   for (svd->its=1;svd->its<svd->max_it;svd->its++) {
     n = svd->n - svd->nconv;
-    for (i=svd->nconv;i<n;i++) {
+    for (i=svd->nconv;i<svd->n;i++) {
       ierr = MatMult(svd->A,V[i],U[i]);CHKERRQ(ierr);
       ierr = cgs2(U[i],i,U);CHKERRQ(ierr);
       ierr = VecNormalize(U[i],alpha+i-svd->nconv);CHKERRQ(ierr);
@@ -130,20 +128,20 @@ PetscErrorCode SVDSolve_LANCZOS(SVD svd)
     dbdsqr_("U",&n,&n,&n,&zero,alpha,beta,PT,&n,Q,&n,PETSC_NULL,&n,work,&info,1);
 
     k = svd->nconv;
-    for (i=svd->nconv;i<n;i++) {
-      svd->sigma[i] = alpha[i];
+    for (i=svd->nconv;i<svd->n;i++) {
+      svd->sigma[i] = alpha[i-svd->nconv];
       
       ierr = VecSet(svd->V[i],0.0);CHKERRQ(ierr);
       for (j=0;j<n;j++) {
-	ierr = VecAXPY(svd->V[i],PT[j*n+i-svd->nconv],V[j]);CHKERRQ(ierr);
+	ierr = VecAXPY(svd->V[i],PT[j*n+i-svd->nconv],V[j+svd->nconv]);CHKERRQ(ierr);
       }
       
       ierr = VecSet(svd->U[i],0.0);CHKERRQ(ierr);
-      ierr = VecMAXPY(svd->U[i],n,Q+(i-svd->nconv)*n,U);CHKERRQ(ierr);
+      ierr = VecMAXPY(svd->U[i],n,Q+(i-svd->nconv)*n,U+svd->nconv);CHKERRQ(ierr);
       
-      ierr = computeres(svd,svd->sigma[i],svd->U[i],svd->V[i],&norm);CHKERRQ(ierr);
-      printf("[%i] sigma[%i] = %g error = %g\n",svd->its,i,svd->sigma[i],norm);
-      if (norm < svd->tol) {
+      ierr = computeres(svd,svd->sigma[i],svd->U[i],svd->V[i],&norm1,&norm2);CHKERRQ(ierr);
+      printf("[%i] sigma[%i] = %g error = %g,%g\n",svd->its,i,svd->sigma[i],norm1,norm2);
+      if (norm1+norm2 < svd->tol) {
         k++;
       } else break;
     }
