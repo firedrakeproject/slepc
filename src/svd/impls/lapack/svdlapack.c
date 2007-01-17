3329719c91f3ef7c3f15557e7239c3e43bb19a09
Andres Tomas <antodo@upvnet.upv.es>
2007-01-17 11:53:33 +0000

    version sin traspuesta del SVDLAPACK
    Subversion-commit: r1318

--- src/svd/impls/lapack/svdlapack.c
+++ src/svd/impls/lapack/svdlapack.c
@@ -32,48 +32,58 @@ PetscErrorCode SVDSetup_LAPACK(SVD svd)
 PetscErrorCode SVDSolve_LAPACK(SVD svd)
 {
   PetscErrorCode  ierr;
-  PetscInt        M,N;
+  PetscInt        M,N,n;
   Mat             mat;
   PetscScalar     *pU,*pVT,*pmat,*pu,*pv;
   PetscReal       *sigma;
   int             i,j,k;
   
   PetscFunctionBegin;
-  if (svd->A) {
-    ierr = MatConvert(svd->A,MATSEQDENSE,MAT_INITIAL_MATRIX,&mat);CHKERRQ(ierr);
-  } else {
-    ierr = MatTranspose(svd->AT,&mat);CHKERRQ(ierr);
-    ierr = MatConvert(mat,MATSEQDENSE,MAT_REUSE_MATRIX,&mat);CHKERRQ(ierr);    
-  }
-  
+  ierr = MatConvert(svd->OP,MATSEQDENSE,MAT_INITIAL_MATRIX,&mat);CHKERRQ(ierr);
   ierr = MatGetArray(mat,&pmat);CHKERRQ(ierr);
   ierr = MatGetSize(mat,&M,&N);CHKERRQ(ierr);
-  ierr = PetscMalloc(sizeof(PetscScalar)*N*N,&pVT);CHKERRQ(ierr);
-  ierr = PetscMalloc(sizeof(PetscReal)*N,&sigma);CHKERRQ(ierr);
+  if (M>=N) {
+     n = N;
+     pU = PETSC_NULL;
+     ierr = PetscMalloc(sizeof(PetscScalar)*N*N,&pVT);CHKERRQ(ierr);
+  } else {
+     n = M;
+     ierr = PetscMalloc(sizeof(PetscScalar)*M*M,&pU);CHKERRQ(ierr);
+     pVT = PETSC_NULL;
+  }
+  ierr = PetscMalloc(sizeof(PetscReal)*n,&sigma);CHKERRQ(ierr);
   
   ierr = SVDDense(M,N,pmat,sigma,pU,pVT);CHKERRQ(ierr);
 
   /* copy singular vectors */
-  for (i=0;i<N;i++) {
-    if (svd->which == SVD_SMALLEST) k = N - i - 1;
+  for (i=0;i<n;i++) {
+    if (svd->which == SVD_SMALLEST) k = n - i - 1;
     else k = i;
     svd->sigma[k] = sigma[i];
     ierr = VecGetArray(svd->U[k],&pu);CHKERRQ(ierr);
     ierr = VecGetArray(svd->V[k],&pv);CHKERRQ(ierr);
-    for (j=0;j<M;j++) pu[j] = pmat[i*M+j];
-    for (j=0;j<N;j++) pv[j] = pVT[j*N+i];
+    if (M>=N) {
+      for (j=0;j<M;j++) pu[j] = pmat[i*M+j];
+      for (j=0;j<N;j++) pv[j] = pVT[j*N+i];
+    } else {
+      for (j=0;j<N;j++) pu[j] = pmat[j*M+i];
+      for (j=0;j<M;j++) pv[j] = pU[i*M+j];
+    }
     ierr = VecRestoreArray(svd->U[k],&pu);CHKERRQ(ierr);
     ierr = VecRestoreArray(svd->V[k],&pv);CHKERRQ(ierr);
   }
 
-  svd->nconv = N;
+  svd->nconv = n;
   svd->reason = SVD_CONVERGED_TOL;
 
   ierr = MatRestoreArray(mat,&pmat);CHKERRQ(ierr);
   ierr = MatDestroy(mat);CHKERRQ(ierr);
   ierr = PetscFree(sigma);CHKERRQ(ierr);
-  ierr = PetscFree(pVT);CHKERRQ(ierr);
-  ierr = PetscFree(pU);CHKERRQ(ierr);
+  if (M>=N) {
+     ierr = PetscFree(pVT);CHKERRQ(ierr);
+  } else {
+     ierr = PetscFree(pU);CHKERRQ(ierr);
+  }
   PetscFunctionReturn(0);
 }
 
