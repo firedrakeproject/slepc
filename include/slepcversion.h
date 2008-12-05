379a3abb2db0445aaf4ff9224970cfc4470b93db
Jose E. Roman <jroman@dsic.upv.es>
2008-12-05 16:45:26 +0000

    Nueva macro SLEPC_VERSION_
    Subversion-commit: r1519

--- include/slepcversion.h
+++ include/slepcversion.h
@@ -25,4 +25,12 @@
 #define SLEPC_AUTHOR_INFO        "        The SLEPc Team\n\
    slepc-maint@grycap.upv.es\n\
  http://www.grycap.upv.es/slepc\n"
+
+#define SLEPC_VERSION_(MAJOR,MINOR,SUBMINOR) \
+ ((SLEPC_VERSION_MAJOR == (MAJOR)) &&       \
+  (SLEPC_VERSION_MINOR == (MINOR)) &&       \
+  (SLEPC_VERSION_SUBMINOR == (SUBMINOR)) && \
+  (SLEPC_VERSION_RELEASE  == 1))
+
 #endif
+
