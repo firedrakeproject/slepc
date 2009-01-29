10ac630ea922213a928b69353d4d440987eabbe6
Andres Tomas <antodo@upvnet.upv.es>
2009-01-29 11:28:35 +0000

    eliminar soporte para compilacion sin MPI y bug en el nombre de las funciones para precision simple
    Subversion-commit: r1650

--- config/arpack.py
+++ config/arpack.py
@@ -16,41 +16,21 @@ import check
 
 def Check(conf,directory,libs):
 
-  if petscconf.MPIUNI:
-
-    if petscconf.SCALAR == 'real':
-      if petscconf.PRECISION == 'single':
-	functions = ['snaupd','sneupd','dsaupd','dseupd']
-      else:
-	functions = ['dnaupd','dneupd','dsaupd','dseupd']
+  if petscconf.SCALAR == 'real':
+    if petscconf.PRECISION == 'single':
+      functions = ['psnaupd','psneupd','pssaupd','psseupd']
     else:
-      if petscconf.PRECISION == 'single':
-	functions = ['cnaupd','cneupd']
-      else:
-	functions = ['znaupd','zneupd']
-
-    if libs:
-      libs = [libs]
-    else:
-      libs = [['-larpack'],['-larpack_LINUX'],['-larpack_SUN4']]
-
+      functions = ['pdnaupd','pdneupd','pdsaupd','pdseupd']
   else:
-
-    if petscconf.SCALAR == 'real':
-      if petscconf.PRECISION == 'single':
-	functions = ['psnaupd','psneupd','pdsaupd','pdseupd']
-      else:
-	functions = ['pdnaupd','pdneupd','pdsaupd','pdseupd']
+    if petscconf.PRECISION == 'single':
+      functions = ['pcnaupd','pcneupd']
     else:
-      if petscconf.PRECISION == 'single':
-	functions = ['pcnaupd','pcneupd']
-      else:
-	functions = ['pznaupd','pzneupd']
+      functions = ['pznaupd','pzneupd']
 
-    if libs:
-      libs = [libs]
-    else:
-      libs = [['-lparpack','-larpack'],['-lparpack_MPI','-larpack'],['-lparpack_MPI-LINUX','-larpack_LINUX'],['-lparpack_MPI-SUN4','-larpack_SUN4']]
+  if libs:
+    libs = [libs]
+  else:
+    libs = [['-lparpack','-larpack'],['-lparpack_MPI','-larpack'],['-lparpack_MPI-LINUX','-larpack_LINUX'],['-lparpack_MPI-SUN4','-larpack_SUN4']]
 
   if directory:
     dirs = [directory]
