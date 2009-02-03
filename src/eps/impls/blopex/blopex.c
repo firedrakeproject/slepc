12edd8f192243ad368aeedf73e1570f88d362293
Andres Tomas <antodo@upvnet.upv.es>
2009-02-03 12:52:37 +0000

    licencia LGPL
    Subversion-commit: r1672

--- src/eps/impls/blopex/blopex.c
+++ src/eps/impls/blopex/blopex.c
@@ -2,11 +2,22 @@
        This file implements a wrapper to the BLOPEX solver
 
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-      SLEPc - Scalable Library for Eigenvalue Problem Computations
-      Copyright (c) 2002-2007, Universidad Politecnica de Valencia, Spain
+   SLEPc - Scalable Library for Eigenvalue Problem Computations
+   Copyright (c) 2002-2009, Universidad Politecnica de Valencia, Spain
 
-      This file is part of SLEPc. See the README file for conditions of use
-      and additional information.
+   This file is part of SLEPc.
+      
+   SLEPc is free software: you can redistribute it and/or modify it under  the
+   terms of version 3 of the GNU Lesser General Public License as published by
+   the Free Software Foundation.
+
+   SLEPc  is  distributed in the hope that it will be useful, but WITHOUT  ANY 
+   WARRANTY;  without even the implied warranty of MERCHANTABILITY or  FITNESS 
+   FOR  A  PARTICULAR PURPOSE. See the GNU Lesser General Public  License  for 
+   more details.
+
+   You  should have received a copy of the GNU Lesser General  Public  License
+   along with SLEPc. If not, see <http://www.gnu.org/licenses/>.
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 */
 
