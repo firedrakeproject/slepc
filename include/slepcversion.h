ab6240dc487defbcf1b8eb192c1c54d2e89bf72a
Jose E. Roman <jroman@dsic.upv.es>
2009-10-30 17:18:33 +0000

    Remove comments from slepcversion.h so that it can be included from Fortran
    Subversion-commit: r1835

--- include/slepcversion.h
+++ include/slepcversion.h
@@ -1,31 +1,6 @@
-/*
-   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   SLEPc - Scalable Library for Eigenvalue Problem Computations
-   Copyright (c) 2002-2009, Universidad Politecnica de Valencia, Spain
-
-   This file is part of SLEPc.
-      
-   SLEPc is free software: you can redistribute it and/or modify it under  the
-   terms of version 3 of the GNU Lesser General Public License as published by
-   the Free Software Foundation.
-
-   SLEPc  is  distributed in the hope that it will be useful, but WITHOUT  ANY 
-   WARRANTY;  without even the implied warranty of MERCHANTABILITY or  FITNESS 
-   FOR  A  PARTICULAR PURPOSE. See the GNU Lesser General Public  License  for 
-   more details.
-
-   You  should have received a copy of the GNU Lesser General  Public  License
-   along with SLEPc. If not, see <http://www.gnu.org/licenses/>.
-   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-*/
-
 #if !defined(__SLEPCVERSION_H)
 #define __SLEPCVERSION_H
 
-/* ========================================================================== */
-/* 
-   Current SLEPC version number and release date
-*/
 #define SLEPC_VERSION_RELEASE    0
 #define SLEPC_VERSION_MAJOR      3
 #define SLEPC_VERSION_MINOR      0
