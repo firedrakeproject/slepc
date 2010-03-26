3007aa840c3ce2ec3954514b90af1491993d5bea
Jose E. Roman <jroman@dsic.upv.es>
2010-03-26 16:40:31 +0000

    Added new QEP example and binary files for coefficient matrices
    Subversion-commit: r1921

---
+++ src/examples/ex17.c
@@ -0,0 +1,175 @@
+/*
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   SLEPc - Scalable Library for Eigenvalue Problem Computations
+   Copyright (c) 2002-2009, Universidad Politecnica de Valencia, Spain
+
+   This file is part of SLEPc.
+      
+   SLEPc is free software: you can redistribute it and/or modify it under  the
+   terms of version 3 of the GNU Lesser General Public License as published by
+   the Free Software Foundation.
+
+   SLEPc  is  distributed in the hope that it will be useful, but WITHOUT  ANY 
+   WARRANTY;  without even the implied warranty of MERCHANTABILITY or  FITNESS 
+   FOR  A  PARTICULAR PURPOSE. See the GNU Lesser General Public  License  for 
+   more details.
+
+   You  should have received a copy of the GNU Lesser General  Public  License
+   along with SLEPc. If not, see <http://www.gnu.org/licenses/>.
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+
+static char help[] = "Solves a quadratic eigenproblem (l^2*M + l*C + K)*x = 0 with matrices loaded from a file.\n\n"
+  "The command line options are:\n"
+  "  -M <filename>, where <filename> = matrix (M) file in PETSc binary form.\n"
+  "  -C <filename>, where <filename> = matrix (C) file in PETSc binary form.\n"
+  "  -K <filename>, where <filename> = matrix (K) file in PETSc binary form.\n\n";
+
+#include "slepcqep.h"
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main( int argc, char **argv )
+{
+  Mat         	 M, C, K;         /* problem matrices */
+  QEP         	 qep;             /* quadratic eigenproblem solver context */
+  const QEPType  type;
+  PetscReal   	 error, tol, re, im;
+  PetscScalar 	 kr, ki;
+  PetscErrorCode ierr;
+  PetscInt    	 nev, maxit, i, its, nconv;
+  char        	 filename[256];
+  PetscViewer 	 viewer;
+  PetscTruth  	 flg;
+
+  SlepcInitialize(&argc,&argv,(char*)0,help);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        Load the matrices that define the quadratic eigenproblem
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\nQuadratic eigenproblem stored in file.\n\n");CHKERRQ(ierr);
+#if defined(PETSC_USE_COMPLEX)
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Reading COMPLEX matrices from binary files...\n");CHKERRQ(ierr);
+#else
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Reading REAL matrices from binary files...\n");CHKERRQ(ierr);
+#endif
+
+  ierr = PetscOptionsGetString(PETSC_NULL,"-M",filename,256,&flg);CHKERRQ(ierr);
+  if (!flg) {
+    SETERRQ(1,"Must indicate a file name for matrix M with the -M option.");
+  }
+  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,filename,FILE_MODE_READ,&viewer);CHKERRQ(ierr);
+  ierr = MatLoad(viewer,MATAIJ,&M);CHKERRQ(ierr);
+  ierr = PetscViewerDestroy(viewer);CHKERRQ(ierr);
+
+  ierr = PetscOptionsGetString(PETSC_NULL,"-C",filename,256,&flg);CHKERRQ(ierr);
+  if (!flg) {
+    SETERRQ(1,"Must indicate a file name for matrix C with the -C option.");
+  }
+  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,filename,FILE_MODE_READ,&viewer);CHKERRQ(ierr);
+  ierr = MatLoad(viewer,MATAIJ,&C);CHKERRQ(ierr);
+  ierr = PetscViewerDestroy(viewer);CHKERRQ(ierr);
+
+  ierr = PetscOptionsGetString(PETSC_NULL,"-K",filename,256,&flg);CHKERRQ(ierr);
+  if (!flg) {
+    SETERRQ(1,"Must indicate a file name for matrix K with the -K option.");
+  }
+  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,filename,FILE_MODE_READ,&viewer);CHKERRQ(ierr);
+  ierr = MatLoad(viewer,MATAIJ,&K);CHKERRQ(ierr);
+  ierr = PetscViewerDestroy(viewer);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                Create the eigensolver and set various options
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* 
+     Create eigensolver context
+  */
+  ierr = QEPCreate(PETSC_COMM_WORLD,&qep);CHKERRQ(ierr);
+
+  /* 
+     Set matrices
+  */
+  ierr = QEPSetOperators(qep,M,C,K);CHKERRQ(ierr);
+
+  /*
+     Set solver parameters at runtime
+  */
+  ierr = QEPSetFromOptions(qep);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                      Solve the eigensystem
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  ierr = QEPSolve(qep);CHKERRQ(ierr);
+  ierr = QEPGetIterationNumber(qep, &its);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of iterations of the method: %d\n",its);CHKERRQ(ierr);
+
+  /*
+     Optional: Get some information from the solver and display it
+  */
+  ierr = QEPGetType(qep,&type);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Solution method: %s\n\n",type);CHKERRQ(ierr);
+  ierr = QEPGetDimensions(qep,&nev,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of requested eigenvalues: %d\n",nev);CHKERRQ(ierr);
+  ierr = QEPGetTolerances(qep,&tol,&maxit);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Stopping condition: tol=%.4g, maxit=%d\n",tol,maxit);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                    Display solution and clean up
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* 
+     Get number of converged approximate eigenpairs
+  */
+  ierr = QEPGetConverged(qep,&nconv);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of converged approximate eigenpairs: %d\n\n",nconv);
+         CHKERRQ(ierr);
+
+  if (nconv>0) {
+    /*
+       Display eigenvalues and relative errors
+    */
+    ierr = PetscPrintf(PETSC_COMM_WORLD,
+         "           k          ||(k^2M+Ck+K)x||/||kx||\n"
+         "   ----------------- -------------------------\n" );CHKERRQ(ierr);
+
+    for( i=0; i<nconv; i++ ) {
+      /* 
+        Get converged eigenpairs: i-th eigenvalue is stored in kr (real part) and
+        ki (imaginary part)
+      */
+      ierr = QEPGetEigenpair(qep,i,&kr,&ki,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
+      /*
+         Compute the relative error associated to each eigenpair
+      */
+      ierr = QEPComputeRelativeError(qep,i,&error);CHKERRQ(ierr);
+
+#ifdef PETSC_USE_COMPLEX
+      re = PetscRealPart(kr);
+      im = PetscImaginaryPart(kr);
+#else
+      re = kr;
+      im = ki;
+#endif 
+      if (im!=0.0) {
+        ierr = PetscPrintf(PETSC_COMM_WORLD," %9f%+9f j    %12g\n",re,im,error);CHKERRQ(ierr);
+      } else {
+        ierr = PetscPrintf(PETSC_COMM_WORLD,"   %12f       %12g\n",re,error);CHKERRQ(ierr); 
+      }
+    }
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"\n" );CHKERRQ(ierr);
+  }
+  
+  /* 
+     Free work space
+  */
+  ierr = QEPDestroy(qep);CHKERRQ(ierr);
+  ierr = MatDestroy(M);CHKERRQ(ierr);
+  ierr = MatDestroy(C);CHKERRQ(ierr);
+  ierr = MatDestroy(K);CHKERRQ(ierr);
+  ierr = SlepcFinalize();CHKERRQ(ierr);
+  return 0;
+}
+
