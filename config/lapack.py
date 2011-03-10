ac58de8b0ea982147f968cb326e2a19d43aff421
Jose E. Roman <jroman@dsic.upv.es>
2011-03-10 15:23:15 +0000

    Add missing Lapack routines in configure test
    Subversion-commit: r2272

--- config/lapack.py
+++ config/lapack.py
@@ -31,17 +31,17 @@ def Check(conf,vars,cmake):
   log.Println('Checking LAPACK library...')
 
   # LAPACK standard functions
-  l = ['laev2','gehrd','lanhs','lange','getri','hseqr','trexc','trevc','geevx','ggevx','gelqf','gesdd','tgexc','gges']
+  l = ['laev2','gehrd','lanhs','lange','getri','hseqr','trexc','trevc','geevx','ggevx','gelqf','gesdd','tgexc','gges','tgevc','ggev']
 
   # LAPACK functions with different real and complex versions
   if petscconf.SCALAR == 'real':
-    l += ['orghr','syevr','sygvd','ormlq']
+    l += ['orghr','syevr','sygvd','ormlq','orgqr']
     if petscconf.PRECISION == 'single':
       prefix = 's'
     else:
       prefix = 'd'
   else:
-    l += ['unghr','heevr','hegvd','unmlq','ungtr','hetrd']
+    l += ['unghr','heevr','hegvd','unmlq','ungtr','hetrd','ungqr']
     if petscconf.PRECISION == 'single':
       prefix = 'c'
     else:
