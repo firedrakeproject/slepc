fc9f0a0603e935f64cc78ac2747cebb3049988fd
Eloy Romero Alcalde <elroal@upvnet.upv.es>
2011-07-22 10:06:21 +0000

    Added a test to solve a QEP without calling QEPSetFromOptions.
    Subversion-commit: r2457

---
+++ src/qep/examples/tests/test1.c
@@ -0,0 +1,198 @@
+/*
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   SLEPc - Scalable Library for Eigenvalue Problem Computations
+   Copyright (c) 2002-2010, Universidad Politecnica de Valencia, Spain
+
+   This file is part of SLEPc.
+      
+   SLEPc is free software: you can redistribute it and/or modify it under  the
+   terms of version 3 of the GNU Lesser General Public License as published by
+   the Free Software Foundation.
+
+   SLEPc  is  distributed in the hope that it will be useful, but WITHOUT  ANY 
+   WARRANTY;  without even the implied warranty of MERCHANTABILITY or  FITNESS 
+   FOR  A  PARTICULAR PURPOSE. See the GNU Lesser General Public  License  for 
+   more details.
+
+   You  should have received a copy of the GNU Lesser General  Public  License
+   along with SLEPc. If not, see <http://www.gnu.org/licenses/>.
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+
+static char help[] = "Test the solution of a QEP without calling QEPSetFromOptions (based on ex16.c).\n\n"
+  "The command line options are:\n"
+  "  -n <n>, where <n> = number of grid subdivisions in x dimension.\n"
+  "  -m <m>, where <m> = number of grid subdivisions in y dimension.\n"
+  "  -type <qep_type> = qep type to test.\n"
+  "  -epstype <eps_type> = eps type to test (for linear).\n\n";
+
+#include <slepcqep.h>
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **argv)
+{
+  Mat            M,C,K;           /* problem matrices */
+  QEP            qep;             /* quadratic eigenproblem solver context */
+  const QEPType  type;
+  PetscReal      error,tol,re,im;
+  PetscScalar    kr,ki;
+  PetscInt       N,n=10,m,Istart,Iend,II,nev,maxit,i,j,its,nconv;
+  PetscBool      flag;
+  char           qeptype[30] = "cross", epstype[30] = "";
+  EPS            eps;
+  PetscErrorCode ierr;
+
+  SlepcInitialize(&argc,&argv,(char*)0,help);
+
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-n",&n,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-m",&m,&flag);CHKERRQ(ierr);
+  if(!flag) m=n;
+  N = n*m;
+  ierr = PetscOptionsGetString(PETSC_NULL,"-type",qeptype,30,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetString(PETSC_NULL,"-epstype",epstype,30,&flag);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\nQuadratic Eigenproblem, N=%d (%dx%d grid)",N,n,m);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\nQEP type: %s",qeptype);CHKERRQ(ierr);
+  if (flag) {
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"\nEPS type: %s",epstype);CHKERRQ(ierr);
+  }
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\n\n");CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     Compute the matrices that define the eigensystem, (k^2*K+k*X+M)x=0
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* K is the 2-D Laplacian */
+  ierr = MatCreate(PETSC_COMM_WORLD,&K);CHKERRQ(ierr);
+  ierr = MatSetSizes(K,PETSC_DECIDE,PETSC_DECIDE,N,N);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(K);CHKERRQ(ierr);
+  
+  ierr = MatGetOwnershipRange(K,&Istart,&Iend);CHKERRQ(ierr);
+  for (II=Istart;II<Iend;II++) { 
+    i = II/n; j = II-i*n;  
+    if(i>0) { ierr = MatSetValue(K,II,II-n,-1.0,INSERT_VALUES);CHKERRQ(ierr); }
+    if(i<m-1) { ierr = MatSetValue(K,II,II+n,-1.0,INSERT_VALUES);CHKERRQ(ierr); }
+    if(j>0) { ierr = MatSetValue(K,II,II-1,-1.0,INSERT_VALUES);CHKERRQ(ierr); }
+    if(j<n-1) { ierr = MatSetValue(K,II,II+1,-1.0,INSERT_VALUES);CHKERRQ(ierr); }
+    ierr = MatSetValue(K,II,II,4.0,INSERT_VALUES);CHKERRQ(ierr);
+  }
+
+  ierr = MatAssemblyBegin(K,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(K,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+
+  /* C is the zero matrix */
+  ierr = MatCreate(PETSC_COMM_WORLD,&C);CHKERRQ(ierr);
+  ierr = MatSetSizes(C,PETSC_DECIDE,PETSC_DECIDE,N,N);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(C);CHKERRQ(ierr);
+  ierr = MatAssemblyBegin(C,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(C,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  
+  /* M is the identity matrix */
+  ierr = MatCreate(PETSC_COMM_WORLD,&M);CHKERRQ(ierr);
+  ierr = MatSetSizes(M,PETSC_DECIDE,PETSC_DECIDE,N,N);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(M);CHKERRQ(ierr);
+  ierr = MatAssemblyBegin(M,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(M,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatShift(M,1.0);CHKERRQ(ierr);
+  
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                Create the eigensolver and set various options
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* 
+     Create eigensolver context
+  */
+  ierr = QEPCreate(PETSC_COMM_WORLD,&qep);CHKERRQ(ierr);
+
+  /* 
+     Set matrices and problem type
+  */
+  ierr = QEPSetOperators(qep,M,C,K);CHKERRQ(ierr);
+  ierr = QEPSetProblemType(qep,QEP_GENERAL);CHKERRQ(ierr);
+
+  /*
+     Set solver parameters at runtime
+  */
+  ierr = QEPSetType(qep,qeptype);CHKERRQ(ierr);
+  if (flag) {
+    ierr = PetscTypeCompare((PetscObject)qep,QEPLINEAR,&flag);CHKERRQ(ierr);
+    if (flag) {
+      ierr = QEPLinearGetEPS(qep,&eps);CHKERRQ(ierr);
+      ierr = EPSSetType(eps,epstype);CHKERRQ(ierr);
+    }
+  }
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                      Solve the eigensystem
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  ierr = QEPSolve(qep);CHKERRQ(ierr);
+
+  /*
+     Optional: Get some information from the solver and display it
+  */
+  ierr = QEPGetIterationNumber(qep,&its);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of iterations of the method: %d\n",its);CHKERRQ(ierr);
+  ierr = QEPGetType(qep,&type);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Solution method: %s\n\n",type);CHKERRQ(ierr);
+  ierr = QEPGetDimensions(qep,&nev,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of requested eigenvalues: %d\n",nev);CHKERRQ(ierr);
+  ierr = QEPGetTolerances(qep,&tol,&maxit);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Stopping condition: tol=%.4g, maxit=%d\n",tol,maxit);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                    Display solution and clean up
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* 
+     Get number of converged approximate eigenpairs
+  */
+  ierr = QEPGetConverged(qep,&nconv);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of converged approximate eigenpairs: %d\n\n",nconv);CHKERRQ(ierr);
+
+  if (nconv>0) {
+    /*
+       Display eigenvalues and relative errors
+    */
+    ierr = PetscPrintf(PETSC_COMM_WORLD,
+         "           k          ||(k^2M+Ck+K)x||/||kx||\n"
+         "   ----------------- -------------------------\n");CHKERRQ(ierr);
+
+    for (i=0;i<nconv;i++) {
+      /* 
+        Get converged eigenpairs: i-th eigenvalue is stored in kr (real part) and
+        ki (imaginary part)
+      */
+      ierr = QEPGetEigenpair(qep,i,&kr,&ki,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
+      /*
+         Compute the relative error associated to each eigenpair
+      */
+      ierr = QEPComputeRelativeError(qep,i,&error);CHKERRQ(ierr);
+
+#if defined(PETSC_USE_COMPLEX)
+      re = PetscRealPart(kr);
+      im = PetscImaginaryPart(kr);
+#else
+      re = kr;
+      im = ki;
+#endif 
+      if (im!=0.0) {
+        ierr = PetscPrintf(PETSC_COMM_WORLD," %9f%+9f j    %12g\n",re,im,error);CHKERRQ(ierr);
+      } else {
+        ierr = PetscPrintf(PETSC_COMM_WORLD,"   %12f       %12g\n",re,error);CHKERRQ(ierr); 
+      }
+    }
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"\n");CHKERRQ(ierr);
+  }
+  
+  /* 
+     Free work space
+  */
+  ierr = QEPDestroy(&qep);CHKERRQ(ierr);
+  ierr = MatDestroy(&M);CHKERRQ(ierr);
+  ierr = MatDestroy(&C);CHKERRQ(ierr);
+  ierr = MatDestroy(&K);CHKERRQ(ierr);
+  ierr = SlepcFinalize();CHKERRQ(ierr);
+  return 0;
+}
+
