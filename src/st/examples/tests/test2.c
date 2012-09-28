a0c25b3a171de6c90f41b6bda541fd50b98ea615
Jose E. Roman <jroman@dsic.upv.es>
2012-09-28 08:37:46 +0000

    Tests for ST
    Subversion-commit: r3097

---
+++ src/st/examples/tests/test2.c
@@ -0,0 +1,157 @@
+/*
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   SLEPc - Scalable Library for Eigenvalue Problem Computations
+   Copyright (c) 2002-2012, Universitat Politecnica de Valencia, Spain
+
+   This file is part of SLEPc.
+      
+   SLEPc is free software: you can redistribute it and/or modify it under  the
+   terms of version 3 of the GNU Lesser General Public License as published by
+   the Free Software Foundation.
+
+   SLEPc  is  distributed in the hope that it will be useful, but WITHOUT  ANY 
+   WARRANTY;  without even the implied warranty of MERCHANTABILITY or  FITNESS 
+   FOR  A  PARTICULAR PURPOSE. See the GNU Lesser General Public  License  for 
+   more details.
+
+   You  should have received a copy of the GNU Lesser General  Public  License
+   along with SLEPc. If not, see <http://www.gnu.org/licenses/>.
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+
+static char help[] = "Test ST with one matrix.\n\n";
+
+#include <slepcst.h>
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **argv)
+{
+  Mat             A,B,mat[1];
+  ST              st;
+  Vec             v,w;
+  const STType    type;
+  PetscScalar     value[3],sigma,tau;
+  PetscInt        n=10,i,Istart,Iend,col[3];
+  PetscBool       FirstBlock=PETSC_FALSE,LastBlock=PETSC_FALSE;
+  PetscErrorCode  ierr;
+
+  SlepcInitialize(&argc,&argv,(char*)0,help);
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-n",&n,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\n1-D Laplacian, n=%D\n\n",n);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     Compute the operator matrix for the 1-D Laplacian
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
+  ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,n,n);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(A);CHKERRQ(ierr);
+  ierr = MatSetUp(A);CHKERRQ(ierr);
+  
+  ierr = MatGetOwnershipRange(A,&Istart,&Iend);CHKERRQ(ierr);
+  if (Istart==0) FirstBlock=PETSC_TRUE;
+  if (Iend==n) LastBlock=PETSC_TRUE;
+  value[0]=-1.0; value[1]=2.0; value[2]=-1.0;
+  for (i=(FirstBlock? Istart+1: Istart); i<(LastBlock? Iend-1: Iend); i++) {
+    col[0]=i-1; col[1]=i; col[2]=i+1;
+    ierr = MatSetValues(A,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+  if (LastBlock) {
+    i=n-1; col[0]=n-2; col[1]=n-1;
+    ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+  if (FirstBlock) {
+    i=0; col[0]=0; col[1]=1; value[0]=2.0; value[1]=-1.0;
+    ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+
+  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatGetVecs(A,&v,&w);CHKERRQ(ierr);
+  ierr = VecSet(v,1.0);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                Create the spectral transformation object
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  ierr = STCreate(PETSC_COMM_WORLD,&st);CHKERRQ(ierr);
+  mat[0] = A;
+  ierr = STSetOperators(st,1,mat);CHKERRQ(ierr);
+  ierr = STSetFromOptions(st);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+              Apply the transformed operator for several ST's
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* shift, sigma=0.0 */
+  ierr = STSetUp(st);CHKERRQ(ierr);
+  ierr = STGetType(st,&type);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"ST type %s\n",type);CHKERRQ(ierr);
+  ierr = STApply(st,v,w);CHKERRQ(ierr);
+  ierr = VecView(w,PETSC_NULL);CHKERRQ(ierr);
+
+  /* shift, sigma=0.1 */
+  sigma = 0.1;
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"With shift=%g\n",sigma);CHKERRQ(ierr);
+  ierr = STSetShift(st,sigma);CHKERRQ(ierr);
+  ierr = STApply(st,v,w);CHKERRQ(ierr);
+  ierr = VecView(w,PETSC_NULL);CHKERRQ(ierr);
+
+  /* sinvert, sigma=0.1 */
+  ierr = STSetType(st,STSINVERT);CHKERRQ(ierr);
+  ierr = STGetType(st,&type);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"ST type %s\n",type);CHKERRQ(ierr);
+  ierr = STGetShift(st,&sigma);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"With shift=%g\n",sigma);CHKERRQ(ierr);
+  ierr = STApply(st,v,w);CHKERRQ(ierr);
+  ierr = VecView(w,PETSC_NULL);CHKERRQ(ierr);
+
+  /* sinvert, sigma=-0.5 */
+  sigma = -0.5;
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"With shift=%g\n",sigma);CHKERRQ(ierr);
+  ierr = STSetShift(st,sigma);CHKERRQ(ierr);
+  ierr = STApply(st,v,w);CHKERRQ(ierr);
+  ierr = VecView(w,PETSC_NULL);CHKERRQ(ierr);
+
+  /* cayley, sigma=-0.5, tau=-0.5 (equal to sigma by default) */
+  ierr = STSetType(st,STCAYLEY);CHKERRQ(ierr);
+  ierr = STGetType(st,&type);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"ST type %s\n",type);CHKERRQ(ierr);
+  ierr = STGetShift(st,&sigma);CHKERRQ(ierr);
+  ierr = STCayleyGetAntishift(st,&tau);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"With shift=%g, antishift=%g\n",sigma,tau);CHKERRQ(ierr);
+  ierr = STApply(st,v,w);CHKERRQ(ierr);
+  ierr = VecView(w,PETSC_NULL);CHKERRQ(ierr);
+
+  /* cayley, sigma=1.1, tau=-0.5 */
+  sigma = 1.1;
+  ierr = STCayleyGetAntishift(st,&tau);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"With shift=%g, antishift=%g\n",sigma,tau);CHKERRQ(ierr);
+  ierr = STSetShift(st,sigma);CHKERRQ(ierr);
+  ierr = STApply(st,v,w);CHKERRQ(ierr);
+  ierr = VecView(w,PETSC_NULL);CHKERRQ(ierr);
+
+  /* cayley, sigma=1.1, tau=-1.0 */
+  tau = -1.0;
+  ierr = STCayleySetAntishift(st,tau);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"With shift=%g, antishift=%g\n",sigma,tau);CHKERRQ(ierr);
+  ierr = STSetShift(st,sigma);CHKERRQ(ierr);
+  ierr = STApply(st,v,w);CHKERRQ(ierr);
+  ierr = VecView(w,PETSC_NULL);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                  Check inner product matrix in Cayley
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  ierr = STGetBilinearForm(st,&B);CHKERRQ(ierr);
+  ierr = MatMult(B,v,w);CHKERRQ(ierr);
+  ierr = VecView(w,PETSC_NULL);CHKERRQ(ierr);
+
+  ierr = STDestroy(&st);CHKERRQ(ierr);
+  ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = MatDestroy(&B);CHKERRQ(ierr);
+  ierr = VecDestroy(&v);CHKERRQ(ierr);
+  ierr = VecDestroy(&w);CHKERRQ(ierr);
+  ierr = SlepcFinalize();CHKERRQ(ierr);
+  return 0;
+}
