firedrakeproject/slepc

commit 81d59cf1cb9f6fa4c469cf21bbcd8e525022b2fc

Option --with-dynamic-loading has been removed.
Also PETSC_USE_DYNAMIC_LIBRARIES has been renamed to
PETSC_HAVE_DYNAMIC_LIBRARIES

src/sys/slepcinit.c
@@ -109,7 +109,7 @@ PetscErrorCode SlepcPrintHelpIntro(MPI_Comm comm)
 PetscBool SlepcBeganPetsc = PETSC_FALSE;
 PetscBool SlepcInitializeCalled = PETSC_FALSE;
 
-#if defined(PETSC_USE_DYNAMIC_LIBRARIES)
+#if defined(PETSC_HAVE_DYNAMIC_LIBRARIES)
 
 #undef __FUNCT__
 #define __FUNCT__ "SlepcInitialize_DynamicLibraries"
@@ -190,7 +190,7 @@ PetscErrorCode SlepcInitialize(int *argc,char ***args,const char file[],const ch
     SlepcBeganPetsc = PETSC_TRUE;
   }
 
-#if defined(PETSC_USE_DYNAMIC_LIBRARIES)
+#if defined(PETSC_HAVE_DYNAMIC_LIBRARIES)
   ierr = SlepcInitialize_DynamicLibraries();CHKERRQ(ierr);
 #endif
   ierr = SlepcInitialize_LogEvents();CHKERRQ(ierr);
@@ -297,7 +297,7 @@ PetscErrorCode SlepcInitializeNoPointers(int argc,char **args,const char *filena
   PetscFunctionReturn(ierr);
 }
 
-#if defined(PETSC_USE_DYNAMIC_LIBRARIES)
+#if defined(PETSC_HAVE_DYNAMIC_LIBRARIES)
 
 #undef __FUNCT__
 #define __FUNCT__ "PetscDLLibraryRegister_slepc"
@@ -327,5 +327,5 @@ PETSC_EXTERN PetscErrorCode PetscDLLibraryRegister_slepc(char *path)
   ierr = FNInitializePackage();CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
-#endif /* PETSC_USE_DYNAMIC_LIBRARIES */
+#endif /* PETSC_HAVE_DYNAMIC_LIBRARIES */
 

src/ds/interface/dsbasic.c
@@ -132,10 +132,7 @@ PetscErrorCode DSCreate(MPI_Comm comm,DS *newds)
   PetscFunctionBegin;
   PetscValidPointer(newds,2);
   *newds = 0;
-#if !defined(PETSC_USE_DYNAMIC_LIBRARIES)
   ierr = DSInitializePackage();CHKERRQ(ierr);
-#endif
-
   ierr = SlepcHeaderCreate(ds,_p_DS,struct _DSOps,DS_CLASSID,"DS","Direct Solver (or Dense System)","DS",comm,DSDestroy,DSView);CHKERRQ(ierr);
 
   ds->state         = DS_STATE_RAW;

src/eps/interface/basic.c
@@ -432,10 +432,7 @@ PetscErrorCode EPSCreate(MPI_Comm comm,EPS *outeps)
   PetscFunctionBegin;
   PetscValidPointer(outeps,2);
   *outeps = 0;
-#if !defined(PETSC_USE_DYNAMIC_LIBRARIES)
   ierr = EPSInitializePackage();CHKERRQ(ierr);
-#endif
-
   ierr = SlepcHeaderCreate(eps,_p_EPS,struct _EPSOps,EPS_CLASSID,"EPS","Eigenvalue Problem Solver","EPS",comm,EPSDestroy,EPSView);CHKERRQ(ierr);
 
   eps->max_it          = 0;

src/fn/fnbasic.c
@@ -119,10 +119,7 @@ PetscErrorCode FNCreate(MPI_Comm comm,FN *newfn)
   PetscFunctionBegin;
   PetscValidPointer(newfn,2);
   *newfn = 0;
-#if !defined(PETSC_USE_DYNAMIC_LIBRARIES)
   ierr = FNInitializePackage();CHKERRQ(ierr);
-#endif
-
   ierr = SlepcHeaderCreate(fn,_p_FN,struct _FNOps,FN_CLASSID,"FN","Math Function","FN",comm,FNDestroy,FNView);CHKERRQ(ierr);
   fn->na       = 0;
   fn->alpha    = NULL;

src/ip/ipbasic.c
@@ -128,10 +128,7 @@ PetscErrorCode IPCreate(MPI_Comm comm,IP *newip)
   PetscFunctionBegin;
   PetscValidPointer(newip,2);
   *newip = 0;
-#if !defined(PETSC_USE_DYNAMIC_LIBRARIES)
   ierr = IPInitializePackage();CHKERRQ(ierr);
-#endif
-
   ierr = SlepcHeaderCreate(ip,_p_IP,struct _IPOps,IP_CLASSID,"IP","Inner Product","IP",comm,IPDestroy,IPView);CHKERRQ(ierr);
 
   ip->orthog_type   = IP_ORTHOG_CGS;

src/mfn/interface/mfnbasic.c
@@ -202,10 +202,7 @@ PetscErrorCode MFNCreate(MPI_Comm comm,MFN *outmfn)
   PetscFunctionBegin;
   PetscValidPointer(outmfn,2);
   *outmfn = 0;
-#if !defined(PETSC_USE_DYNAMIC_LIBRARIES)
   ierr = MFNInitializePackage();CHKERRQ(ierr);
-#endif
-
   ierr = SlepcHeaderCreate(mfn,_p_MFN,struct _MFNOps,MFN_CLASSID,"MFN","Matrix Function","MFN",comm,MFNDestroy,MFNView);CHKERRQ(ierr);
 
   mfn->max_it          = 0;

src/nep/interface/nepbasic.c
@@ -248,10 +248,7 @@ PetscErrorCode NEPCreate(MPI_Comm comm,NEP *outnep)
   PetscFunctionBegin;
   PetscValidPointer(outnep,2);
   *outnep = 0;
-#if !defined(PETSC_USE_DYNAMIC_LIBRARIES)
   ierr = NEPInitializePackage();CHKERRQ(ierr);
-#endif
-
   ierr = SlepcHeaderCreate(nep,_p_NEP,struct _NEPOps,NEP_CLASSID,"NEP","Nonlinear Eigenvalue Problem","NEP",comm,NEPDestroy,NEPView);CHKERRQ(ierr);
 
   nep->max_it          = 0;

src/qep/interface/qepbasic.c
@@ -364,10 +364,7 @@ PetscErrorCode QEPCreate(MPI_Comm comm,QEP *outqep)
   PetscFunctionBegin;
   PetscValidPointer(outqep,2);
   *outqep = 0;
-#if !defined(PETSC_USE_DYNAMIC_LIBRARIES)
   ierr = QEPInitializePackage();CHKERRQ(ierr);
-#endif
-
   ierr = SlepcHeaderCreate(qep,_p_QEP,struct _QEPOps,QEP_CLASSID,"QEP","Quadratic Eigenvalue Problem","QEP",comm,QEPDestroy,QEPView);CHKERRQ(ierr);
 
   qep->M               = 0;

commit 525d8d13a11005f5ffd6e7db2c2169db35cf6910

Fix initialization problem in case of static libraries
The problem was introduced in 81d59cf.
The fix includes the run-time option -dynamic_library_preload
as in PETSc.

src/sys/slepcinit.c
@@ -112,6 +112,24 @@ PetscBool SlepcInitializeCalled = PETSC_FALSE;
 #if defined(PETSC_HAVE_DYNAMIC_LIBRARIES)
 
 #undef __FUNCT__
+#define __FUNCT__ "SlepcLoadDynamicLibrary"
+static PetscErrorCode SlepcLoadDynamicLibrary(const char *name,PetscBool *found)
+{
+  char           libs[PETSC_MAX_PATH_LEN],dlib[PETSC_MAX_PATH_LEN];
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = PetscStrcpy(libs,SLEPC_LIB_DIR);CHKERRQ(ierr);
+  ierr = PetscStrcat(libs,"/libslepc");CHKERRQ(ierr);
+  ierr = PetscStrcat(libs,name);CHKERRQ(ierr);
+  ierr = PetscDLLibraryRetrieve(PETSC_COMM_WORLD,libs,dlib,1024,found);CHKERRQ(ierr);
+  if (*found) {
+    ierr = PetscDLLibraryAppend(PETSC_COMM_WORLD,&PetscDLLibrariesLoaded,dlib);CHKERRQ(ierr);
+  }
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
 #define __FUNCT__ "SlepcInitialize_DynamicLibraries"
 /*
     SlepcInitialize_DynamicLibraries - Adds the default dynamic link libraries to the
@@ -121,15 +139,15 @@ PetscErrorCode SlepcInitialize_DynamicLibraries(void)
 {
   PetscErrorCode ierr;
   PetscBool      found;
-  char           libs[PETSC_MAX_PATH_LEN],dlib[PETSC_MAX_PATH_LEN];
+  PetscBool      preload;
 
   PetscFunctionBegin;
-  ierr = PetscStrcpy(libs,SLEPC_LIB_DIR);CHKERRQ(ierr);
-  ierr = PetscStrcat(libs,"/libslepc");CHKERRQ(ierr);
-  ierr = PetscDLLibraryRetrieve(PETSC_COMM_WORLD,libs,dlib,1024,&found);CHKERRQ(ierr);
-  if (found) {
-    ierr = PetscDLLibraryAppend(PETSC_COMM_WORLD,&PetscDLLibrariesLoaded,libs);CHKERRQ(ierr);
-  } else SETERRQ(PETSC_COMM_SELF,PETSC_ERR_FILE_OPEN,"Unable to locate SLEPc dynamic library\nYou cannot move the dynamic libraries");
+  preload = PETSC_FALSE;
+  ierr = PetscOptionsGetBool(NULL,"-dynamic_library_preload",&preload,NULL);CHKERRQ(ierr);
+  if (preload) {
+    ierr = SlepcLoadDynamicLibrary("",&found);CHKERRQ(ierr);
+    if (!found) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_FILE_OPEN,"Unable to locate SLEPc dynamic library\nYou cannot move the dynamic libraries!");
+  }
   PetscFunctionReturn(0);
 }
 #endif