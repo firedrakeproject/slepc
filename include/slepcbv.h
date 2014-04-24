4aad6f681e618067fda396391595f70aa8f227d1
Jose E. Roman <jroman@dsic.upv.es>
2014-04-24 15:39:12 +0200

    BV: add missing prototype of functions added in 5f20686

--- include/slepcbv.h
+++ include/slepcbv.h
@@ -65,6 +65,8 @@ PETSC_EXTERN PetscErrorCode BVView(BV,PetscViewer);
 PETSC_EXTERN PetscErrorCode BVGetColumn(BV,PetscInt,Vec*);
 PETSC_EXTERN PetscErrorCode BVRestoreColumn(BV,PetscInt,Vec*);
 PETSC_EXTERN PetscErrorCode BVGetVec(BV,Vec*);
+PETSC_EXTERN PetscErrorCode BVSetActiveColumns(BV,PetscInt,PetscInt);
+PETSC_EXTERN PetscErrorCode BVGetActiveColumns(BV,PetscInt*,PetscInt*);
 
 PETSC_EXTERN PetscErrorCode BVMult(BV,PetscScalar,PetscScalar,BV,Mat);
 PETSC_EXTERN PetscErrorCode BVMultVec(BV,PetscScalar,PetscScalar,Vec,PetscScalar*);
