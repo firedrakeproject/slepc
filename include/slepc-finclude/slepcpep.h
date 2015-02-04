firedrakeproject/slepc

commit 0ac48c9eb8145ca70ccf32509b71b1e970522eb4

Fortran interface: add missing definition of enumerate

include/slepc-finclude/slepcpep.h
@@ -106,6 +106,14 @@
       parameter (PEP_EXTRACT_RESIDUAL       =  3)
       parameter (PEP_EXTRACT_STRUCTURED     =  4)
 
+      PetscEnum PEP_ERROR_ABSOLUTE
+      PetscEnum PEP_ERROR_RELATIVE
+      PetscEnum PEP_ERROR_BACKWARD
+
+      parameter (PEP_ERROR_ABSOLUTE       =  1)
+      parameter (PEP_ERROR_RELATIVE       =  2)
+      parameter (PEP_ERROR_BACKWARD       =  3)
+
       PetscEnum PEP_CONV_ABS
       PetscEnum PEP_CONV_EIG
       PetscEnum PEP_CONV_LINEAR

include/slepc-finclude/slepcpepdef.h
@@ -41,6 +41,7 @@
 #define PEPRefine          PetscEnum
 #define PEPExtract         PetscEnum
 #define PEPConv            PetscEnum
+#define PEPErrorType       PetscEnum
 #define PEPConvergedReason PetscEnum
 
 #define PEPLINEAR    'linear'

commit b4f3bcfcbd3337f461ca90b5ffe7d11aceb191d8

Fix wrong numbers introduced in 0ac48c9

include/slepc-finclude/slepcpep.h
@@ -110,9 +110,9 @@
       PetscEnum PEP_ERROR_RELATIVE
       PetscEnum PEP_ERROR_BACKWARD
 
-      parameter (PEP_ERROR_ABSOLUTE       =  1)
-      parameter (PEP_ERROR_RELATIVE       =  2)
-      parameter (PEP_ERROR_BACKWARD       =  3)
+      parameter (PEP_ERROR_ABSOLUTE         =  0)
+      parameter (PEP_ERROR_RELATIVE         =  1)
+      parameter (PEP_ERROR_BACKWARD         =  2)
 
       PetscEnum PEP_CONV_ABS
       PetscEnum PEP_CONV_EIG