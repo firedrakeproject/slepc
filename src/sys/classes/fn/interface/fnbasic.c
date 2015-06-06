2ab47795a1a0cfd37d513cd4e2dcdc14f01a9098
Jose E. Roman <jroman@dsic.upv.es>
2015-06-06 19:00:00 +0200

    Reorganize files in subdirectories under fn

--- src/sys/classes/fn/interface/fnbasic.c
+++ src/sys/classes/fn/interface/fnbasic.c
@@ -797,35 +797,3 @@ PetscErrorCode FNRegister(const char *name,PetscErrorCode (*function)(FN))
   PetscFunctionReturn(0);
 }
 
-PETSC_EXTERN PetscErrorCode FNCreate_Combine(FN);
-PETSC_EXTERN PetscErrorCode FNCreate_Rational(FN);
-PETSC_EXTERN PetscErrorCode FNCreate_Exp(FN);
-PETSC_EXTERN PetscErrorCode FNCreate_Log(FN);
-PETSC_EXTERN PetscErrorCode FNCreate_Phi(FN);
-PETSC_EXTERN PetscErrorCode FNCreate_Sqrt(FN);
-
-#undef __FUNCT__
-#define __FUNCT__ "FNRegisterAll"
-/*@C
-   FNRegisterAll - Registers all of the math functions in the FN package.
-
-   Not Collective
-
-   Level: advanced
-@*/
-PetscErrorCode FNRegisterAll(void)
-{
-  PetscErrorCode ierr;
-
-  PetscFunctionBegin;
-  if (FNRegisterAllCalled) PetscFunctionReturn(0);
-  FNRegisterAllCalled = PETSC_TRUE;
-  ierr = FNRegister(FNCOMBINE,FNCreate_Combine);CHKERRQ(ierr);
-  ierr = FNRegister(FNRATIONAL,FNCreate_Rational);CHKERRQ(ierr);
-  ierr = FNRegister(FNEXP,FNCreate_Exp);CHKERRQ(ierr);
-  ierr = FNRegister(FNLOG,FNCreate_Log);CHKERRQ(ierr);
-  ierr = FNRegister(FNPHI,FNCreate_Phi);CHKERRQ(ierr);
-  ierr = FNRegister(FNSQRT,FNCreate_Sqrt);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
-
