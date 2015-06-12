c6a1b4efe3f09bf9f85eeec76bbf61aec054c631
Jose E. Roman <jroman@dsic.upv.es>
2015-06-12 16:28:18 +0200

    Update copyright year

--- src/sys/classes/ds/impls/pep/dspep.c
+++ src/sys/classes/ds/impls/pep/dspep.c
@@ -1,7 +1,7 @@
 /*
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    SLEPc - Scalable Library for Eigenvalue Problem Computations
-   Copyright (c) 2002-2014, Universitat Politecnica de Valencia, Spain
+   Copyright (c) 2002-2015, Universitat Politecnica de Valencia, Spain
 
    This file is part of SLEPc.
 
