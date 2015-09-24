996005ab6852783d859e884e8d9f2b1456e1bd12
Yasuyuki Maeda <maeda@mma.cs.tsukuba.ac.jp>
2015-09-24 14:26:08 +0900

    EPS: fix determination of L_max in CISS

--- src/eps/impls/ciss/ciss.c
+++ src/eps/impls/ciss/ciss.c
@@ -727,9 +727,16 @@ PetscErrorCode EPSSetUp_CISS(EPS eps)
   Mat            A;
 
   PetscFunctionBegin;
-  ierr = EPSSetDimensions_Default(eps,eps->nev,&eps->ncv,&eps->mpd);CHKERRQ(ierr);
-  ctx->L_max = (int)eps->ncv/ctx->M;
-  if (ctx->L > ctx->L_max)ctx->L = ctx->L_max;
+  if (eps->ncv == 0) eps->ncv = ctx->L_max*ctx->M;
+  else {
+    ierr = EPSSetDimensions_Default(eps,eps->nev,&eps->ncv,&eps->mpd);CHKERRQ(ierr);
+    ctx->L_max = (int)eps->ncv/ctx->M;
+    if (ctx->L_max == 0) { 
+      ctx->L_max = 1;
+      eps->ncv = ctx->L_max*ctx->M;
+    }
+    if (ctx->L > ctx->L_max)ctx->L = ctx->L_max;
+  }
   if (!eps->mpd) eps->mpd = eps->ncv;
   if (!eps->which) eps->which = EPS_ALL;
   if (!eps->extraction) { ierr = EPSSetExtraction(eps,EPS_RITZ);CHKERRQ(ierr); } 
