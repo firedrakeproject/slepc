fec14b815d29aaaf1f05bd0733eb682fa2358e1f
Jose E. Roman <jroman@dsic.upv.es>
2016-02-26 10:59:08 +0100

    NEP: show relative errors in the gun example

--- src/nep/examples/nlevp/gun.c
+++ src/nep/examples/nlevp/gun.c
@@ -141,7 +141,7 @@ int main(int argc,char **argv)
   } else {
     ierr = PetscViewerPushFormat(PETSC_VIEWER_STDOUT_WORLD,PETSC_VIEWER_ASCII_INFO_DETAIL);CHKERRQ(ierr);
     ierr = NEPReasonView(nep,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
-    ierr = NEPErrorView(nep,NEP_ERROR_ABSOLUTE,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+    ierr = NEPErrorView(nep,NEP_ERROR_RELATIVE,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
     ierr = PetscViewerPopFormat(PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
   }
   ierr = NEPDestroy(&nep);CHKERRQ(ierr);
