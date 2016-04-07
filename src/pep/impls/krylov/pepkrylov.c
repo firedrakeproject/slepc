b9b3b4c8ddfcb43315b31f7a96b8fda8ee9740ae
Jose E. Roman <jroman@dsic.upv.es>
2016-04-07 10:19:09 +0200

    Revert one change of 58e11eb

--- src/pep/impls/krylov/pepkrylov.c
+++ src/pep/impls/krylov/pepkrylov.c
@@ -141,6 +141,7 @@ PetscErrorCode PEPExtractVectors_TOAR(PEP pep)
 #endif
         t = 0.0;
         ierr = PEPEvaluateBasis(pep,er[i],ei[i],vals,ivals);CHKERRQ(ierr);
+        yr = X+i*ldds; yi = NULL;
         for (j=0;j<deg;j++) {
           alpha = PetscConj(vals[j]);
 #if !defined(PETSC_USE_COMPLEX)
@@ -165,7 +166,7 @@ PetscErrorCode PEPExtractVectors_TOAR(PEP pep)
         }
         t = 1.0/t;
         PetscStackCallBLAS("BLASscal",BLASscal_(&k_,&t,SS+i*k,&one));
-        if (ei[i]!=0.0) {
+        if (yi) {
           PetscStackCallBLAS("BLASscal",BLASscal_(&k_,&t,SS+(i+1)*k,&one));
           i++;
         }
