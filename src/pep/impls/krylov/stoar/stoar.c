eb2f3bff447fe193f0fa77b19de5ee9fcbf3e01e
Carmen Campos <mccampos@dsic.upv.es>
2016-05-20 21:20:22 +0200

    PEPSTOAR: set linearization flag

--- src/pep/impls/krylov/stoar/stoar.c
+++ src/pep/impls/krylov/stoar/stoar.c
@@ -129,6 +129,7 @@ PetscErrorCode PEPSetUp_STOAR(PEP pep)
   PetscReal      norm,*omega;
 
   PetscFunctionBegin;
+  pep->lineariz = PETSC_TRUE;
   ierr = PEPSetDimensions_Default(pep,pep->nev,&pep->ncv,&pep->mpd);CHKERRQ(ierr);
   if (!ctx->lock && pep->mpd<pep->ncv) SETERRQ(PetscObjectComm((PetscObject)pep),PETSC_ERR_SUP,"Should not use mpd parameter in non-locking variant");
   if (!pep->max_it) pep->max_it = PetscMax(100,2*pep->n/pep->ncv);
