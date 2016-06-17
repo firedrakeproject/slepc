firedrakeproject/slepc

commit 49d7ffa3619c23d4d7ffa2643ac73a843b4640ce

EPS: in slicing change the sweep direction in the last subcommunicator

src/eps/impls/krylov/krylovschur/ks-slice.c
@@ -366,6 +366,7 @@ PetscErrorCode EPSSetUp_KrylovSchur_Slice(EPS eps)
   EPS_SR          sr,sr_loc,sr_glob;
   PetscInt        nEigs,dssz=1,i,zeros=0,off=0;
   PetscMPIInt     nproc,rank,aux;
+  PetscReal       r;
   MPI_Request     req;
   Mat             A,B=NULL;
 
@@ -502,6 +503,8 @@ PetscErrorCode EPSSetUp_KrylovSchur_Slice(EPS eps)
     if (ctx->npart==1 || ((sr->dir>0 && ctx->subc->color==ctx->npart-1) || (sr->dir<0 && ctx->subc->color==0))) {
       ierr = EPSSliceGetInertia(eps,sr->int1,&sr->inertia1,ctx->detect?&zeros:NULL);CHKERRQ(ierr);
       if (zeros) SETERRQ(((PetscObject)eps)->comm,PETSC_ERR_USER,"Found singular matrix for the transformed problem in an interval endpoint defined by user");
+      sr->dir = -sr->dir; r = sr->int0; sr->int0 = sr->int1; sr->int1 = r;
+      i = sr->inertia0; sr->inertia0 = sr->inertia1; sr->inertia1 = i;
     }
 
     /* number of eigenvalues in interval */

commit 56b85aee35c47e86d5b62ba08159fd0917de00d3

EPS: in slicing fix the exchange of info between subcommunicators
Problem introduced in 49d7ffa

src/eps/impls/krylov/krylovschur/ks-slice.c
@@ -436,13 +436,13 @@ PetscErrorCode EPSSetUp_KrylovSchur_Slice(EPS eps)
       if (nproc%ctx->npart==0) { /* subcommunicators with the same size */
         ierr = PetscMPIIntCast(sr_loc->numEigs,&aux);CHKERRQ(ierr);
         ierr = MPI_Allgather(&aux,1,MPI_INT,ctx->nconv_loc,1,MPI_INT,ctx->commrank);CHKERRQ(ierr);
-        ierr = MPI_Allgather(&sr_loc->int0,1,MPIU_REAL,ctx->subintervals+off,1,MPIU_REAL,ctx->commrank);CHKERRQ(ierr);
+        ierr = MPI_Allgather(sr_loc->dir==sr->dir?&sr_loc->int0:&sr_loc->int1,1,MPIU_REAL,ctx->subintervals+off,1,MPIU_REAL,ctx->commrank);CHKERRQ(ierr);
       } else {
         ierr = MPI_Comm_rank(PetscSubcommChild(ctx->subc),&rank);CHKERRQ(ierr);
         if (!rank) {
           ierr = PetscMPIIntCast(sr_loc->numEigs,&aux);CHKERRQ(ierr);
           ierr = MPI_Allgather(&aux,1,MPI_INT,ctx->nconv_loc,1,MPI_INT,ctx->commrank);CHKERRQ(ierr);
-          ierr = MPI_Allgather(&sr_loc->int0,1,MPIU_REAL,ctx->subintervals+off,1,MPIU_REAL,ctx->commrank);CHKERRQ(ierr);
+          ierr = MPI_Allgather(sr_loc->dir==sr->dir?&sr_loc->int0:&sr_loc->int1,1,MPIU_REAL,ctx->subintervals+off,1,MPIU_REAL,ctx->commrank);CHKERRQ(ierr);
         }
         ierr = PetscMPIIntCast(ctx->npart,&aux);CHKERRQ(ierr);
         ierr = MPI_Bcast(ctx->nconv_loc,aux,MPI_INT,0,PetscSubcommChild(ctx->subc));CHKERRQ(ierr);