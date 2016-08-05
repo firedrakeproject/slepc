1924138ca8acccbed09ab1a797dab2732dc4f140
Jose E. Roman <jroman@dsic.upv.es>
2016-08-05 14:06:12 +0200

    Nightly tests: new target testexamples_C_DataFile

--- config/configure.py
+++ config/configure.py
@@ -264,6 +264,8 @@ testruns = set(petsc.test_runs.split())
 testruns = testruns.intersection(set(['C','F90','Fortran','C_Complex','Fortran_Complex','C_NoComplex','F90_NoComplex','Fortran_NoComplex','C_NotSingle','C_NoComplex_NotSingle','VECCUDA','VECCUDA_Complex','VECCUDA_NoComplex']))
 if petsc.precision == 'double':
   testruns = testruns.union(set(['C_Double']))
+if petsc.precision == 'double' and petsc.scalar == 'real' and not petsc.ind64:
+  testruns = testruns.union(set(['C_DataFile']))
 if slepc.datadir:
   slepcvars.write('DATAFILESPATH = '+slepc.datadir+'\n')
   if petsc.scalar == 'complex':
