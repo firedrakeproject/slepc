2a1e8fffce5ed4699fd4cacdf25c51036858f16c
Jose E. Roman <jroman@dsic.upv.es>
2016-08-20 20:39:34 +0200

    FN: add new tests for the log and phi functions

---
+++ src/sys/classes/fn/examples/tests/test10.c
@@ -0,0 +1,89 @@
+/*
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   SLEPc - Scalable Library for Eigenvalue Problem Computations
+   Copyright (c) 2002-2016, Universitat Politecnica de Valencia, Spain
+
+   This file is part of SLEPc.
+
+   SLEPc is free software: you can redistribute it and/or modify it under  the
+   terms of version 3 of the GNU Lesser General Public License as published by
+   the Free Software Foundation.
+
+   SLEPc  is  distributed in the hope that it will be useful, but WITHOUT  ANY
+   WARRANTY;  without even the implied warranty of MERCHANTABILITY or  FITNESS
+   FOR  A  PARTICULAR PURPOSE. See the GNU Lesser General Public  License  for
+   more details.
+
+   You  should have received a copy of the GNU Lesser General  Public  License
+   along with SLEPc. If not, see <http://www.gnu.org/licenses/>.
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+
+static char help[] = "Test Phi functions.\n\n";
+
+#include <slepcfn.h>
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **argv)
+{
+  PetscErrorCode ierr;
+  FN             phi0,phi1,phik;
+  PetscInt       k;
+  PetscScalar    x,y,yp,tau,eta;
+  char           strx[50],str[50];
+
+  SlepcInitialize(&argc,&argv,(char*)0,help);
+
+  /* phi_0(x) = exp(x) */
+  ierr = FNCreate(PETSC_COMM_WORLD,&phi0);CHKERRQ(ierr);
+  ierr = FNSetType(phi0,FNPHI);CHKERRQ(ierr);
+  ierr = FNPhiSetIndex(phi0,0);CHKERRQ(ierr);
+  ierr = FNView(phi0,NULL);CHKERRQ(ierr);
+  x = 2.2;
+  ierr = SlepcSNPrintfScalar(strx,50,x,PETSC_FALSE);CHKERRQ(ierr);
+  ierr = FNEvaluateFunction(phi0,x,&y);CHKERRQ(ierr);
+  ierr = FNEvaluateDerivative(phi0,x,&yp);CHKERRQ(ierr);
+  ierr = SlepcSNPrintfScalar(str,50,y,PETSC_FALSE);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"  f(%s)=%s\n",strx,str);CHKERRQ(ierr);
+  ierr = SlepcSNPrintfScalar(str,50,yp,PETSC_FALSE);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"  f'(%s)=%s\n",strx,str);CHKERRQ(ierr);
+
+  /* phi_1(x) = (exp(x)-1)/x with scaling factors eta*log(tau*x) */
+  ierr = FNCreate(PETSC_COMM_WORLD,&phi1);CHKERRQ(ierr);
+  ierr = FNSetType(phi1,FNPHI);CHKERRQ(ierr);  /* default index should be 1 */
+  tau = 0.2;
+  eta = 1.3;
+  ierr = FNSetScale(phi1,tau,eta);CHKERRQ(ierr);
+  ierr = FNView(phi1,NULL);CHKERRQ(ierr);
+  x = 2.2;
+  ierr = SlepcSNPrintfScalar(strx,50,x,PETSC_FALSE);CHKERRQ(ierr);
+  ierr = FNEvaluateFunction(phi1,x,&y);CHKERRQ(ierr);
+  ierr = FNEvaluateDerivative(phi1,x,&yp);CHKERRQ(ierr);
+  ierr = SlepcSNPrintfScalar(str,50,y,PETSC_FALSE);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"  f(%s)=%s\n",strx,str);CHKERRQ(ierr);
+  ierr = SlepcSNPrintfScalar(str,50,yp,PETSC_FALSE);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"  f'(%s)=%s\n",strx,str);CHKERRQ(ierr);
+
+  /* phi_k(x) with index set from command-line arguments */
+  ierr = FNCreate(PETSC_COMM_WORLD,&phik);CHKERRQ(ierr);
+  ierr = FNSetType(phik,FNPHI);CHKERRQ(ierr);
+  ierr = FNSetFromOptions(phik);CHKERRQ(ierr);
+  ierr = FNPhiGetIndex(phik,&k);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Index of phi function is %D\n",k);CHKERRQ(ierr);
+  ierr = FNView(phik,NULL);CHKERRQ(ierr);
+  x = 2.2;
+  ierr = SlepcSNPrintfScalar(strx,50,x,PETSC_FALSE);CHKERRQ(ierr);
+  ierr = FNEvaluateFunction(phik,x,&y);CHKERRQ(ierr);
+  ierr = FNEvaluateDerivative(phik,x,&yp);CHKERRQ(ierr);
+  ierr = SlepcSNPrintfScalar(str,50,y,PETSC_FALSE);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"  f(%s)=%s\n",strx,str);CHKERRQ(ierr);
+  ierr = SlepcSNPrintfScalar(str,50,yp,PETSC_FALSE);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"  f'(%s)=%s\n",strx,str);CHKERRQ(ierr);
+
+  ierr = FNDestroy(&phi0);CHKERRQ(ierr);
+  ierr = FNDestroy(&phi1);CHKERRQ(ierr);
+  ierr = FNDestroy(&phik);CHKERRQ(ierr);
+  ierr = SlepcFinalize();
+  return ierr;
+}
