4f27a9210ee7b078ad2d5f828562b43d7628e948
Jose E. Roman <jroman@dsic.upv.es>
2016-10-19 13:11:22 +0200

    Nightly tests: add an EPSCISS test that sets options in code

---
+++ src/eps/examples/tests/test25.c
@@ -0,0 +1,176 @@
+/*
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   SLEPc - Scalable Library for Eigenvalue Problem Computations
+   Copyright (c) 2002-2016, Universitat Politecnica de Valencia, Spain
+
+   This file is part of SLEPc.
+
+   SLEPc is free software: you can redistribute it and/or modify it under  the
+   terms of version 3 of the GNU Lesser General Public License as published by
+   the Free Software Foundation.
+
+   SLEPc  is  distributed in the hope that it will be useful, but WITHOUT  ANY
+   WARRANTY;  without even the implied warranty of MERCHANTABILITY or  FITNESS
+   FOR  A  PARTICULAR PURPOSE. See the GNU Lesser General Public  License  for
+   more details.
+
+   You  should have received a copy of the GNU Lesser General  Public  License
+   along with SLEPc. If not, see <http://www.gnu.org/licenses/>.
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+
+static char help[] = "Solves a GNHEP problem with contour integral. "
+  "Based on ex5, but with an added B matrix.\n"
+  "The command line options are:\n"
+  "  -m <m>, where <m> = number of grid subdivisions in each dimension.\n\n";
+
+#include <slepceps.h>
+
+/* User-defined routines */
+PetscErrorCode MatMarkovModel(PetscInt m,Mat A);
+PetscErrorCode MatLaplacian(PetscInt m,Mat A);
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **argv)
+{
+  EPS               eps;
+  RG                rg;
+  Mat               A,B;
+  PetscInt          N,m=15;
+  PetscBool         flg;
+  EPSCISSExtraction extr;
+  EPSCISSQuadRule   quad;
+  PetscErrorCode    ierr;
+
+  ierr = SlepcInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
+  ierr = PetscOptionsGetInt(NULL,NULL,"-m",&m,NULL);CHKERRQ(ierr);
+  N = m*(m+1)/2;
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\nGNHEP problem with contour integral, N=%D (m=%D)\n\n",N,m);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                      Compute the matrices
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* A is the Markov matrix */
+  ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
+  ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,N,N);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(A);CHKERRQ(ierr);
+  ierr = MatSetUp(A);CHKERRQ(ierr);
+  ierr = MatMarkovModel(m,A);CHKERRQ(ierr);
+
+  /* A is the 1-D Laplacian matrix */
+  ierr = MatCreate(PETSC_COMM_WORLD,&B);CHKERRQ(ierr);
+  ierr = MatSetSizes(B,PETSC_DECIDE,PETSC_DECIDE,N,N);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(B);CHKERRQ(ierr);
+  ierr = MatSetUp(B);CHKERRQ(ierr);
+  ierr = MatLaplacian(m,B);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                Create the eigensolver and solve the problem
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  ierr = EPSCreate(PETSC_COMM_WORLD,&eps);CHKERRQ(ierr);
+  ierr = EPSSetOperators(eps,A,B);CHKERRQ(ierr);
+  ierr = EPSSetProblemType(eps,EPS_GNHEP);CHKERRQ(ierr);
+  ierr = EPSSetTolerances(eps,1e-9,PETSC_DEFAULT);CHKERRQ(ierr);
+
+  /* set CISS solver with various options */
+  ierr = EPSSetType(eps,EPSCISS);CHKERRQ(ierr);
+  ierr = EPSCISSSetExtraction(eps,EPS_CISS_EXTRACTION_HANKEL);CHKERRQ(ierr);
+  ierr = EPSCISSSetQuadRule(eps,EPS_CISS_QUADRULE_CHEBYSHEV);CHKERRQ(ierr);
+  ierr = EPSCISSSetUseST(eps,PETSC_TRUE);CHKERRQ(ierr);
+  ierr = EPSGetRG(eps,&rg);CHKERRQ(ierr);
+  ierr = RGSetType(rg,RGINTERVAL);CHKERRQ(ierr);
+  ierr = RGIntervalSetEndpoints(rg,0.5,0.6,0.0,0.0);CHKERRQ(ierr);
+
+  ierr = EPSSetFromOptions(eps);CHKERRQ(ierr);
+
+  ierr = EPSSolve(eps);CHKERRQ(ierr);
+  ierr = PetscObjectTypeCompare((PetscObject)eps,EPSCISS,&flg);CHKERRQ(ierr);
+  if (flg) {
+    ierr = EPSCISSGetExtraction(eps,&extr);CHKERRQ(ierr);
+    ierr = EPSCISSGetQuadRule(eps,&quad);CHKERRQ(ierr);
+    ierr = PetscPrintf(PETSC_COMM_WORLD," Solved with CISS using %s extraction with %s quadrature rule\n\n",EPSCISSExtractions[extr],EPSCISSQuadRules[quad]);CHKERRQ(ierr);
+  }
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                    Display solution and clean up
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  ierr = EPSErrorView(eps,EPS_ERROR_BACKWARD,NULL);CHKERRQ(ierr);
+  ierr = EPSDestroy(&eps);CHKERRQ(ierr);
+  ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = MatDestroy(&B);CHKERRQ(ierr);
+  ierr = SlepcFinalize();
+  return ierr;
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatMarkovModel"
+PetscErrorCode MatMarkovModel(PetscInt m,Mat A)
+{
+  const PetscReal cst = 0.5/(PetscReal)(m-1);
+  PetscReal       pd,pu;
+  PetscInt        Istart,Iend,i,j,jmax,ix=0;
+  PetscErrorCode  ierr;
+
+  PetscFunctionBeginUser;
+  ierr = MatGetOwnershipRange(A,&Istart,&Iend);CHKERRQ(ierr);
+  for (i=1;i<=m;i++) {
+    jmax = m-i+1;
+    for (j=1;j<=jmax;j++) {
+      ix = ix + 1;
+      if (ix-1<Istart || ix>Iend) continue;  /* compute only owned rows */
+      if (j!=jmax) {
+        pd = cst*(PetscReal)(i+j-1);
+        /* north */
+        if (i==1) {
+          ierr = MatSetValue(A,ix-1,ix,2*pd,INSERT_VALUES);CHKERRQ(ierr);
+        } else {
+          ierr = MatSetValue(A,ix-1,ix,pd,INSERT_VALUES);CHKERRQ(ierr);
+        }
+        /* east */
+        if (j==1) {
+          ierr = MatSetValue(A,ix-1,ix+jmax-1,2*pd,INSERT_VALUES);CHKERRQ(ierr);
+        } else {
+          ierr = MatSetValue(A,ix-1,ix+jmax-1,pd,INSERT_VALUES);CHKERRQ(ierr);
+        }
+      }
+      /* south */
+      pu = 0.5 - cst*(PetscReal)(i+j-3);
+      if (j>1) {
+        ierr = MatSetValue(A,ix-1,ix-2,pu,INSERT_VALUES);CHKERRQ(ierr);
+      }
+      /* west */
+      if (i>1) {
+        ierr = MatSetValue(A,ix-1,ix-jmax-2,pu,INSERT_VALUES);CHKERRQ(ierr);
+      }
+      /* modification with respect to original Markov model: add a small diagonal value */
+      ierr = MatSetValue(A,ix-1,ix-1,1e-12,INSERT_VALUES);CHKERRQ(ierr);
+    }
+  }
+  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "MatLaplacian"
+PetscErrorCode MatLaplacian(PetscInt n,Mat A)
+{
+  PetscInt       Istart,Iend,i;
+  PetscErrorCode ierr;
+
+  PetscFunctionBeginUser;
+  ierr = MatGetOwnershipRange(A,&Istart,&Iend);CHKERRQ(ierr);
+  for (i=Istart;i<Iend;i++) {
+    if (i>0) { ierr = MatSetValue(A,i,i-1,-1.0,INSERT_VALUES);CHKERRQ(ierr); }
+    if (i<n-1) { ierr = MatSetValue(A,i,i+1,-1.0,INSERT_VALUES);CHKERRQ(ierr); }
+    ierr = MatSetValue(A,i,i,2.0,INSERT_VALUES);CHKERRQ(ierr);
+  }
+  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
