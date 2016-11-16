f037b6c04dff96110aa8e65b185661cb47ffe75b
Jose E. Roman <jroman@dsic.upv.es>
2016-11-16 11:49:38 +0100

    makefile: additional fixes for slepc_alltests rule

--- config/install.py
+++ config/install.py
@@ -276,6 +276,8 @@ for dir in dirs:
 
   def installShare(self):
     self.copies.extend(self.copytree(self.rootShareDir, self.destShareDir))
+    if os.path.exists(os.path.join(self.destShareDir,'slepc','examples')):
+      shutil.rmtree(os.path.join(self.destShareDir,'slepc','examples'))
     os.mkdir(os.path.join(self.destShareDir,'slepc','examples'))
     self.copyExamples(self.rootDir,os.path.join(self.destShareDir,'slepc','examples'))
     self.fixExamplesMakefile(os.path.join(self.destShareDir,'slepc','examples','makefile'))
