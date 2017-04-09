e0d1e0ddec3879f447b89c77aa5fe9b65ac38155
Jose E. Roman <jroman@dsic.upv.es>
2017-04-09 13:48:29 +0200

    FN: in test examples set scaling parameters via -fn_scale

--- src/sys/classes/fn/examples/tests/test3.c
+++ src/sys/classes/fn/examples/tests/test3.c
@@ -78,22 +78,19 @@ int main(int argc,char **argv)
   FN             fn;
   Mat            A;
   PetscInt       i,j,n=10;
-  PetscScalar    *As,tau=1.0,eta=1.0;
+  PetscScalar    *As;
   PetscViewer    viewer;
   PetscBool      verbose,inplace;
 
   ierr = SlepcInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
   ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
-  ierr = PetscOptionsGetScalar(NULL,NULL,"-tau",&tau,NULL);CHKERRQ(ierr);
-  ierr = PetscOptionsGetScalar(NULL,NULL,"-eta",&eta,NULL);CHKERRQ(ierr);
   ierr = PetscOptionsHasName(NULL,NULL,"-verbose",&verbose);CHKERRQ(ierr);
   ierr = PetscOptionsHasName(NULL,NULL,"-inplace",&inplace);CHKERRQ(ierr);
   ierr = PetscPrintf(PETSC_COMM_WORLD,"Matrix exponential, n=%D.\n",n);CHKERRQ(ierr);
 
-  /* Create exponential function eta*exp(tau*x) */
+  /* Create exponential function object */
   ierr = FNCreate(PETSC_COMM_WORLD,&fn);CHKERRQ(ierr);
   ierr = FNSetType(fn,FNEXP);CHKERRQ(ierr);
-  ierr = FNSetScale(fn,tau,eta);CHKERRQ(ierr);
   ierr = FNSetFromOptions(fn);CHKERRQ(ierr);
 
   /* Set up viewer */
