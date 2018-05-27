ad84d19c29c173ed343f3a5498bb5c33c4cd8472
Jose E. Roman <jroman@dsic.upv.es>
2018-05-27 19:04:32 +0200

    NEPNARNOLDI: default max iterations must depend on requested nev

--- src/nep/impls/narnoldi/narnoldi.c
+++ src/nep/impls/narnoldi/narnoldi.c
@@ -37,7 +37,7 @@ PetscErrorCode NEPSetUp_NArnoldi(NEP nep)
   PetscFunctionBegin;
   ierr = NEPSetDimensions_Default(nep,nep->nev,&nep->ncv,&nep->mpd);CHKERRQ(ierr);
   if (nep->ncv>nep->nev+nep->mpd) SETERRQ(PetscObjectComm((PetscObject)nep),1,"The value of ncv must not be larger than nev+mpd");
-  if (!nep->max_it) nep->max_it = nep->ncv;
+  if (!nep->max_it) nep->max_it = nep->nev*nep->ncv;
   if (nep->which && nep->which!=NEP_TARGET_MAGNITUDE) SETERRQ(PetscObjectComm((PetscObject)nep),1,"Wrong value of which");
   if (nep->fui!=NEP_USER_INTERFACE_SPLIT) SETERRQ(PetscObjectComm((PetscObject)nep),PETSC_ERR_SUP,"NARNOLDI only available for split operator");
 
