17dfdd28d4067db782ccd67fbae31b1cb6e3030e
Jose E. Roman <jroman@dsic.upv.es>
2018-09-18 18:44:44 +0200

    Change version number to 3.10

--- include/slepcversion.h
+++ include/slepcversion.h
@@ -1,12 +1,12 @@
 #if !defined(__SLEPCVERSION_H)
 #define __SLEPCVERSION_H
 
-#define SLEPC_VERSION_RELEASE    0
+#define SLEPC_VERSION_RELEASE    1
 #define SLEPC_VERSION_MAJOR      3
-#define SLEPC_VERSION_MINOR      9
-#define SLEPC_VERSION_SUBMINOR   2
+#define SLEPC_VERSION_MINOR      10
+#define SLEPC_VERSION_SUBMINOR   0
 #define SLEPC_VERSION_PATCH      0
-#define SLEPC_RELEASE_DATE       "April 12, 2018"
+#define SLEPC_RELEASE_DATE       "September 18, 2018"
 #define SLEPC_VERSION_DATE       "unknown"
 
 #if !defined (SLEPC_VERSION_GIT)
