41eaea169132330ce57d2e86b08b7835f9e12e45
Jose E. Roman <jroman@dsic.upv.es>
2019-02-06 11:43:32 +0100

    Nightly tests: fix one output file and one test for complex

--- src/nep/examples/tutorials/ex20.c
+++ src/nep/examples/tutorials/ex20.c
@@ -429,10 +429,17 @@ PetscErrorCode FixSign(Vec x)
       filter: sed -e "s/[0-9]\.[0-9]*e-\([0-9]*\)/removed/g" -e "s/ Number of NEP iterations = \([0-9]*\)/ Number of NEP iterations = /"
       requires: !single
 
-   test:
-      suffix: 2
-      args: -nep_type nleigs -rg_interval_endpoints 4,900 -nep_target 10 -nep_nev 4
+   testset:
+      args: -nep_type nleigs -nep_target 10 -nep_nev 4
       filter: sed -e "s/[0-9]\.[0-9]*e-\([0-9]*\)/removed/g" -e "s/0\.\([0-9]*\)/removed/g" -e "s/ Number of NEP iterations = \([0-9]*\)/ Number of NEP iterations = /"
-      requires: !single
+      test:
+         suffix: 2
+         args: -rg_interval_endpoints 4,900
+         requires: !single !complex
+      test:
+         suffix: 2_complex
+         output_file: output/ex20_2.out
+         args: -rg_interval_endpoints 4,900,-.1,.1
+         requires: !single complex
 
 TEST*/
