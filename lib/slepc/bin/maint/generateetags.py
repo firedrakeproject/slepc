398c6c1ddd0c191939cd19b2244db701ea2e9764
Jose E. Roman <jroman@dsic.upv.es>
2019-02-15 10:50:24 +0100

    Update generateetags.py to be closer to PETSc's

--- lib/slepc/bin/maint/generateetags.py
+++ lib/slepc/bin/maint/generateetags.py
@@ -13,52 +13,77 @@ import os
 import re
 import sys
 from string import *
-if sys.version_info < (3,):
-  import commands
-  getstatusoutput = commands.getstatusoutput
-else:
-  import subprocess
-  getstatusoutput = subprocess.getstatusoutput
+import subprocess
+try:
+  from subprocess import check_output
+except ImportError:
+  def check_output(*popenargs, **kwargs):
+    """Implementation from Python-2.7 subprocess.check_output for use with
+    Python-2.6 which does not provide check_output.
+    """
+    if 'stdout' in kwargs:
+      raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+      cmd = kwargs.get("args")
+      if cmd is None:
+        cmd = popenargs[0]
+      raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
 
+DEVNULL = open(os.devnull, 'w')
 #
 #  Copies structs from filename to filename.tmp
 
 def addFileNameTags(filename):
   removedefines = 0
-  f = open(filename)
-  g = open('TAGS','w')
+  f = open(filename, 'rb')
+  g = open('TAGS', 'wb')
   line = f.readline()
   while line:
-    if not (removedefines and line.startswith('#define ')): g.write(line)
-    if line.startswith('\f'):
+    if not (removedefines and line.startswith(b'#define ')): g.write(line)
+    if line.startswith(b'\f'):
       line = f.readline()
       g.write(line)
-      line = line[0:line.index(',')]
-      if os.path.dirname(line).endswith('custom') and not line.endswith('.h'):
+      line = line[0:line.index(b',')]
+      if os.path.dirname(line).endswith(b'custom') and not line.endswith(b'.h'):
         removedefines = 1
       else: removedefines = 0
       line = os.path.basename(line)
-      g.write(line+':^?'+line+'^A,1\n')
+      g.write(line + b':^?' + line + b'^A,1\n')
     line = f.readline()
   f.close()
   g.close()
   return
 
 def createTags(flist,etagfile,ctagfile):
+  # split up the flist into blocks of 1000 - and call etags on each chunk
+  nfiles = len(flist)
+  niter  = nfiles//1000
+  nrem   = nfiles%1000
+  blocks = [i*1000 for i in range(niter+1)]
+  if nrem: blocks.append(nfiles)
+  for i in range(len(blocks)-1):
+    createTagsBlock(flist[blocks[i]:blocks[i+1]],etagfile,ctagfile)
+  return
+
+def createTagsBlock(flist,etagfile,ctagfile):
   # error check for each parameter?
   frlist = [os.path.relpath(path,os.getcwd()) for path in flist]
 
-  (status,output) = getstatusoutput('etags -a -o '+etagfile+' '+' '.join(frlist))
-  if status:
-    raise RuntimeError("Error running etags "+output)
+  subprocess.check_call('etags -a -o '+etagfile+' '+' '.join(frlist), shell=True)
 
   # linux can use '--tag-relative=yes --langmap=c:+.cu'. For others [Mac,bsd] try running ctags in root directory - with relative path to file
   if ctagfile:
-    (status,output) = getstatusoutput('ctags --tag-relative=yes --langmap=c:+.cu  -a -f '+ctagfile+' '+' '.join(frlist))
+    status = subprocess.call('ctags --fields=+l --tag-relative=yes --langmap=c:+.cu  -a -f '+ctagfile+' '+' '.join(frlist), shell=True, stdout=DEVNULL, stderr=subprocess.STDOUT)
     if status:
-      (status,output) = getstatusoutput('ctags -a -f '+ctagfile+' '+' '.join(frlist))
+      status = subprocess.call('/usr/local/bin/ctags -a -f '+ctagfile+' '+' '.join(frlist), shell=True, stdout=DEVNULL, stderr=subprocess.STDOUT)
       if status:
-        raise RuntimeError("Error running ctags "+output)
+        status = subprocess.call('ctags -a -f '+ctagfile+' '+' '.join(frlist), shell=True, stdout=DEVNULL, stderr=subprocess.STDOUT)
+        if status:
+          raise RuntimeError("Error running ctags")
   return
 
 def endsWithSuffix(file,suffixes):
@@ -84,34 +109,34 @@ def badWebIndex(dirname,file):
   else:
     return 1
 
-def processDir(flist,dirname,names):
+def processDir(flist, dirpath, dirnames, filenames):
   newls = []
   gsfx = ['.py','.c','.cu','.F','.F90','.h','.h90','.tex','.cxx','.hh','makefile','.bib','.jl']
   bpfx = ['.#']
   hsfx = ['.html']
   bsfx = ['.py.html','.c.html','.F.html','.h.html','.tex.html','.cxx.html','.hh.html','makefile.html','.gcov.html','.cu.html','.cache.html']
-  for l in names:
+  for l in filenames:
     if endsWithSuffix(l,gsfx) and not startsWithPrefix(l,bpfx):
       newls.append(l)
-    elif endsWithSuffix(l,hsfx)  and not endsWithSuffix(l,bsfx) and not badWebIndex(dirname,l):
+    elif endsWithSuffix(l,hsfx)  and not endsWithSuffix(l,bsfx) and not badWebIndex(dirpath,l):
       # if html - and not bad suffix - and not badWebIndex - then add to etags-list
       newls.append(l)
-  if newls: flist.extend([os.path.join(dirname,name) for name in newls])
+  if newls: flist.extend([os.path.join(dirpath,name) for name in newls])
 
   # exclude 'docs'
   for exname in ['docs']:
-    names.remove(exname)
+    dirnames.remove(exname)
   # One-level unique dirs
   for exname in ['.git','.hg','SCCS', 'output', 'BitKeeper', 'externalpackages', 'bilinear', 'ftn-auto','lib','systems']:
-    if exname in names:
-      names.remove(exname)
+    if exname in dirnames:
+      dirnames.remove(exname)
   # check for configure generated PETSC_ARCHes
   rmnames=[]
-  for name in names:
-    if os.path.isdir(os.path.join(dirname,name,'slepc','conf')):
+  for name in dirnames:
+    if os.path.isdir(os.path.join(dirpath,name,'slepc','conf')):
       rmnames.append(name)
   for rmname in rmnames:
-    names.remove(rmname)
+    dirnames.remove(rmname)
   return
 
 def processFiles(dirname,flist):
@@ -139,11 +164,12 @@ def main(ctags):
   else:
     ctagfile = None
   flist = []
-  (status,output) = getstatusoutput('git ls-files| egrep -v \(^\(docs/\|share/\)\|/output/\|\.\(png\|pdf\|ps\|ppt\|jpg\|tex\)$\)')
-  if not status:
-    flist = output.split('\n')
-  else:
-    os.path.walk(os.getcwd(),processDir,flist)
+  try:
+    output = check_output('git ls-files | egrep -v \(^\(docs/\|share/slepc/datafiles/\)\|/output/\|\.\(png\|pdf\|ps\|ppt\|jpg\|tex\)$\)', shell=True)
+    flist = output.decode(sys.getfilesystemencoding()).splitlines()
+  except OSError:
+    for dirpath, dirnames, filenames in os.walk(os.getcwd()):
+      processDir(flist, dirpath, dirnames, filenames)
     processFiles(os.getcwd(),flist)
   createTags(flist,etagfile,ctagfile)
   addFileNameTags(etagfile)
