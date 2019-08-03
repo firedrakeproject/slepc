d24cad03d232c437a14f3cb9fc2fe648c16cc9e0
Jose E. Roman <jroman@dsic.upv.es>
2019-08-03 20:51:26 +0200

    Nightly tests: more alt files for complex scalars

--- src/pep/examples/nlevp/spring.c
+++ src/pep/examples/nlevp/spring.c
@@ -171,7 +171,7 @@ int main(int argc,char **argv)
    test:
       suffix: 5
       args: -n 300 -pep_hyperbolic -pep_interval -.506,-.3 -pep_type stoar -st_type sinvert -st_pc_type cholesky -pep_stoar_nev 10 -terse
-      requires: !single
+      requires: !complex !single
 
    test:
       suffix: 6
