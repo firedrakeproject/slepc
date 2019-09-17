376189b31f21613c80bac763a94d3f9eee81a237
Carmen Campos <mccampos@dsic.upv.es>
2019-09-17 17:19:13 +0200

    EPS slice: sync the child and parent objects so that both have the same direction

--- src/eps/impls/krylov/krylovschur/ks-slice.c
+++ src/eps/impls/krylov/krylovschur/ks-slice.c
@@ -415,6 +415,7 @@ PetscErrorCode EPSSetUp_KrylovSchur_Slice(EPS eps)
     /* create subintervals and initialize auxiliary eps for slicing runs */
     ierr = EPSSliceGetEPS(eps);CHKERRQ(ierr);
     sr_loc = ((EPS_KRYLOVSCHUR*)ctx->eps->data)->sr;
+    sr->dir = sr_loc->dir;
     if (ctx->npart>1) {
       if ((sr->dir>0&&ctx->subc->color==0)||(sr->dir<0&&ctx->subc->color==ctx->npart-1)) sr->inertia0 = sr_loc->inertia0;
       ierr = MPI_Comm_rank(PetscSubcommChild(ctx->subc),&rank);CHKERRQ(ierr);
