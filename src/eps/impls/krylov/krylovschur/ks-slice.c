8c23ed81d59ad439e16d73d9fde7e59d46e394d4
Carmen Campos <mccampos@dsic.upv.es>
2019-12-04 18:18:04 +0100

    EPS: add info messages for spectrum slicing

--- src/eps/impls/krylov/krylovschur/ks-slice.c
+++ src/eps/impls/krylov/krylovschur/ks-slice.c
@@ -339,6 +339,7 @@ static PetscErrorCode EPSSliceGetInertia(EPS eps,PetscReal shift,PetscInt *inert
     ierr = PCFactorGetMatrix(pc,&F);CHKERRQ(ierr);
     ierr = MatGetInertia(F,inertia,zeros,NULL);CHKERRQ(ierr);
   }
+  ierr = PetscInfo2(eps,"Computed inertia at shift %g: %D\n",(double)nzshift,*inertia);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
@@ -932,6 +933,8 @@ static PetscErrorCode EPSKrylovSchur_Slice(EPS eps)
   EPS_shift       sPres;
   PetscBool       breakdown,complIterating,sch0,sch1;
   EPS_SR          sr = ctx->sr;
+#define MSGLEN 100
+  char            messg[MSGLEN];
 
   PetscFunctionBegin;
   /* Spectrum slicing data */
@@ -1089,6 +1092,8 @@ static PetscErrorCode EPSKrylovSchur_Slice(EPS eps)
   }
   sPres->comp[0] = PetscNot(count0 < sPres->nsch[0]);
   sPres->comp[1] = PetscNot(count1 < sPres->nsch[1]);
+  ierr = PetscSNPrintf(messg,MSGLEN,"Lanczos: %D evals in [%g,%g]%s and %D evals in [%g,%g]%s\n",count0,(double)(sr->dir==1)?sPres->ext[0]:sPres->value,(double)(sr->dir==1)?sPres->value:sPres->ext[0],(sPres->comp[0])?"*":"",count1,(double)(sr->dir==1)?sPres->value:sPres->ext[1],(double)(sr->dir==1)?sPres->ext[1]:sPres->value,(sPres->comp[1])?"*":"");CHKERRQ(ierr);
+  ierr = PetscInfo(eps,messg);CHKERRQ(ierr);
   if (count0 > sPres->nsch[0] || count1 > sPres->nsch[1]) InertiaMismatch(eps,ctx->detect);
   ierr = PetscFree(iwork);CHKERRQ(ierr);
   PetscFunctionReturn(0);
