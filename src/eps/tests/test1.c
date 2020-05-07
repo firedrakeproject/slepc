bef61de30b3d70048de7c09424e5652e77696d18
Jose E. Roman <jroman@dsic.upv.es>
2020-05-07 08:40:10 +0200

    CI: fix ciss tests in eps/test1.c
    - there were duplicate tests due to a bad merge
    - reintroduced a cuda test that was unintendedly removed in 7fec4c7d3

--- src/eps/tests/test1.c
+++ src/eps/tests/test1.c
@@ -153,12 +153,6 @@ int main(int argc,char **argv)
          suffix: 1_jd_borth
          args: -eps_type jd -eps_jd_borth
       test:
-         suffix: 1_ciss
-         args: -eps_type ciss -rg_interval_endpoints 20.8,22 -eps_largest_real
-      test:
-         suffix: 1_ciss_trapezoidal
-         args: -eps_type ciss -rg_interval_endpoints 20.8,22 -eps_largest_real -eps_ciss_quadrule trapezoidal -eps_ciss_integration_points 24 -eps_ciss_extraction hankel -eps_ciss_delta 1e-10 -eps_tol 5e-11
-      test:
          suffix: 1_lobpcg
          args: -eps_type lobpcg -st_shift 22 -eps_largest_real
       test:
@@ -244,6 +238,10 @@ int main(int argc,char **argv)
       test:
          suffix: 7_lanczos
          args: -eps_type lanczos -eps_lanczos_reorthog full
+      test:
+         suffix: 7_ciss
+         args: -eps_type ciss -rg_interval_endpoints 20.8,22
+         output_file: output/test1_1_ciss.out
 
    testset:
       args: -n 18 -eps_nev 3 -eps_smallest_real -eps_max_it 500 -st_pc_type sor -mat_type aijcusparse
