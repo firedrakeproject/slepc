445fabc4df5488b933ed9d4ce8f826decbe5400b
Fande Kong <fdkong.jd@gmail.com>
2020-09-14 18:33:27 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/eps/impls/power/power.c
+++ src/eps/impls/power/power.c
@@ -550,9 +550,7 @@ PetscErrorCode EPSSolve_Power(EPS eps)
       ierr = SNESGetConvergedReason(power->snes,&snesreason);CHKERRQ(ierr);
       /* For Newton eigensolver, we are ready to return once SNES converged. */
       if (snesreason>0) eps->nconv = 1;
-
-      /* if relerr<tol, accept eigenpair */
-    } else if (relerr<eps->tol) {
+    } else if (relerr<eps->tol) {   /* accept eigenpair */
       eps->nconv = eps->nconv + 1;
       if (eps->nconv<eps->nev) {
         ierr = EPSGetStartVector(eps,eps->nconv,&breakdown);CHKERRQ(ierr);
