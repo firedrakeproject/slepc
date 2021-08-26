c2b2befbc42fe09d4f88583a42375577e9abc276
Jose E. Roman <jroman@dsic.upv.es>
2021-08-26 08:40:03 +0200

    configure: drop support for Python-2.6

--- config/configure.py
+++ config/configure.py
@@ -13,8 +13,8 @@ from __future__ import print_function
 import os, sys, time, shutil
 
 # Check python version
-if not hasattr(sys, 'version_info') or sys.version_info < (2,6):
-  sys.exit('ERROR: Python version 2.6 or higher is required')
+if not hasattr(sys, 'version_info') or sys.version_info < (2,7):
+  sys.exit('ERROR: Python version 2.7 or higher is required')
 
 def WriteModulesFile(modules,version,sdir):
   ''' Write the contents of the Modules file '''
