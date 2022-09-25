31c472935e18ebdc1cd49eebd42e92ff922047b2
Lisandro Dalcin <dalcinl@gmail.com>
2022-09-25 06:35:05 +0000

    Apply suggestion: add Jose to copyright

--- src/binding/slepc4py/docs/source/conf.py
+++ src/binding/slepc4py/docs/source/conf.py
@@ -59,7 +59,7 @@ main_doc = 'index'
 
 # General information about the project.
 project = u'SLEPc for Python'
-copyright = u'2022, Lisandro Dalcin'
+copyright = u'2022, Lisandro Dalcin and Jose Roman'
 author = u'Lisandro Dalcin'
 
 # The version info for the project you're documenting, acts as replacement for
